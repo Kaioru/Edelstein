72df210fe7ee2a644911988ce9804c0b7d6a18ee
Kaioru <keifuurawrs@gmail.com>
2019-05-12 18:44:30 +0800

    Add skill constants

--- src/Edelstein.Core/Gameplay/Constants/SkillConstants.cs
+++ src/Edelstein.Core/Gameplay/Constants/SkillConstants.cs
@@ -9,6 +9,16 @@ namespace Edelstein.Core.Gameplay.Constants
             return job / 1000 == 3 || job / 100 == 22 || job == 2001;
         }
 
+        public static int GetJobLevel(int job)
+        {
+            if (job % 100 > 0 && job != 2001)
+                return (job / 10 == 43
+                           ? (job - 430) / 2
+                           : job % 10
+                       ) + 2;
+            return 1;
+        }
+
         public static bool IsKeydownSkill(int skill)
         {
             switch ((Skill) skill)
@@ -34,6 +44,61 @@ namespace Edelstein.Core.Gameplay.Constants
                 default:
                     return false;
             }
+        }
+
+        public static bool IsIgnoreMasterLevelForCommon(int skill)
+        {
+            switch ((Skill) skill)
+            {
+                case Skill.HeroCombatMastery:
+                case Skill.PaladinBlessingArmor:
+                case Skill.DarkknightBeholdersRevenge:
+                case Skill.Archmage1MasterMagic:
+                case Skill.Archmage2MasterMagic:
+                case Skill.BishopMasterMagic:
+                case Skill.BowmasterMarkmanShip:
+                case Skill.CrossbowmasterMarkmanShip:
+                case Skill.NightlordExpertJavelin:
+                case Skill.ShadowerGrid:
+                case Skill.ViperCounterAttack:
+                case Skill.CaptainCounterAttack:
+                case Skill.BmageEnergize:
+                case Skill.WildhunterWildInstinct:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsSkillNeedMasterLevel(int skill)
+        {
+            if (IsIgnoreMasterLevelForCommon(skill)) return false;
+            var job = skill / 10000;
+
+            if (job / 100 == 22 || job == 2001)
+            {
+                switch ((Skill) skill)
+                {
+                    case Skill.EvanMagicGuard:
+                    case Skill.EvanMagicBooster:
+                    case Skill.EvanMagicCritical:
+                        return true;
+                }
+
+                return GetJobLevel(job) == 9 || GetJobLevel(job) == 10;
+            }
+
+            switch ((Skill) skill)
+            {
+                case Skill.Dual2SlashStorm:
+                case Skill.Dual3HustleDash:
+                case Skill.Dual4MirrorImaging:
+                case Skill.Dual4FlyingAssaulter:
+                    return true;
+            }
+
+            if (job == 100 * (job / 100)) return false;
+            return GetJobLevel(job) == 4;
         }
     }
 }
