e9a86d0c2f99139f8a38858cfc97c82ae8f539a6
Kaioru <keifuurawrs@gmail.com>
2019-06-08 12:37:18 +0800

    Add giving of EXP from quest act

--- src/Edelstein.Provider/Templates/Quest/QuestActTemplate.cs
+++ src/Edelstein.Provider/Templates/Quest/QuestActTemplate.cs
@@ -2,8 +2,11 @@ namespace Edelstein.Provider.Templates.Quest
 {
     public class QuestActTemplate : QuestOperationTemplate
     {
+        public int EXP { get; }
+
         public QuestActTemplate(int id, IDataProperty property) : base(id, property)
         {
+            EXP = property.Resolve<int>("exp") ?? 0;
         }
     }
 }
