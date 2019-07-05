a3c4d2149729d88cc01b16c223de68dd96d09529
Kaioru <keifuurawrs@gmail.com>
2019-07-05 20:26:17 +0800

    Add input validation

--- src/Edelstein.Service.WebAPI/Types/RegisterInput.cs
+++ src/Edelstein.Service.WebAPI/Types/RegisterInput.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Edelstein.Service.WebAPI.Types
 {
     public class RegisterInput
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        [Required] [MinLength(4)] public string Username { get; set; }
+        [Required] [MinLength(5)] public string Password { get; set; }
     }
 }
