e9baa5a111ca9bf9d90673ad87272acc739fd65d
Kaioru <keith@kaioru.co>
2019-12-30 14:45:13 +0800

    Add character encoding to select world

--- src/Edelstein.Service.Login/Handlers/SelectWorldHandler.cs
+++ src/Edelstein.Service.Login/Handlers/SelectWorldHandler.cs
@@ -1,11 +1,14 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Edelstein.Core.Distributed.States;
+using Edelstein.Core.Gameplay.Extensions.Packets;
 using Edelstein.Core.Utils;
 using Edelstein.Core.Utils.Packets;
 using Edelstein.Entities;
+using Edelstein.Entities.Characters;
 using Edelstein.Network.Packets;
 using Edelstein.Service.Login.Types;
+using MoreLinq;
 
 namespace Edelstein.Service.Login.Handlers
 {
@@ -66,11 +69,25 @@ namespace Edelstein.Service.Login.Handlers
                     if (adapter.Account.LatestConnectedWorld != worldID)
                         adapter.Account.LatestConnectedWorld = worldID;
 
-                    p.Encode<byte>(0); // TODO characters
+                    var characters = store
+                        .Query<Character>()
+                        .Where(c => c.AccountWorldID == accountWorld.ID)
+                        .ToImmutableList();
+
+                    p.Encode<byte>((byte) characters.Count);
+                    characters.ForEach(c =>
+                    {
+                        c.EncodeStats(p);
+                        c.EncodeLook(p);
+
+                        p.Encode<bool>(false);
+                        p.Encode<bool>(false);
+                    });
+
                     p.Encode<bool>(
                         !string.IsNullOrEmpty(adapter.Account.PIC)
                     ); // bLoginOpt TODO: proper bLoginOpt stuff
-                    p.Encode<int>(3); // TODO: nSlotCount
+                    p.Encode<int>(accountWorld.SlotCount);
                     p.Encode<int>(0);
                 }
 
