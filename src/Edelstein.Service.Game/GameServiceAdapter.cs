3fdde92ec891da0b5e6f631326820e417ee51b02
Kaioru <keith@kaioru.co>
2020-01-01 14:02:11 +0800

    Fix users not spawning at nearest spawn point

--- src/Edelstein.Service.Game/GameServiceAdapter.cs
+++ src/Edelstein.Service.Game/GameServiceAdapter.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Edelstein.Core.Services.Migrations;
+using Edelstein.Core.Templates.Fields;
 using Edelstein.Network;
 using Edelstein.Service.Game.Fields.Objects.User;
 
@@ -15,6 +17,30 @@ namespace Edelstein.Service.Game
             GameService service
         ) : base(socket, service)
             => Service = service;
+
+        public override async Task OnUpdate()
+        {
+            if (User.Field.Template.ForcedReturn.HasValue)
+            {
+                Character.FieldID = User.Field.Template.ForcedReturn.Value;
+                Character.FieldPortal = 0;
+            }
+            else
+                Character.FieldPortal = (byte) User.Field.Template.Portals
+                    .Values
+                    .Where(p => p.Type == FieldPortalType.StartPoint)
+                    .OrderBy(p =>
+                    {
+                        var xd = p.Position.X - User.Position.X;
+                        var yd = p.Position.Y - User.Position.Y;
+
+                        return xd * xd + yd * yd;
+                    })
+                    .First()
+                    .ID;
+
+            await base.OnUpdate();
+        }
 
         public override async Task OnDisconnect()
         {
