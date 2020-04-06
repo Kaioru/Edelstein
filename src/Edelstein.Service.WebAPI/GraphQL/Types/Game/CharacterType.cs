6369e7d61fcd74bf7964f554de5b5bb24128ee44
Kaioru <keith@kaioru.co>
2020-04-06 20:25:35 +0800

    Omit unneccesary fields in WebAPI

--- src/Edelstein.Service.WebAPI/GraphQL/Types/Game/CharacterType.cs
+++ src/Edelstein.Service.WebAPI/GraphQL/Types/Game/CharacterType.cs
@@ -21,17 +21,6 @@ namespace Edelstein.Service.WebAPI.GraphQL.Types.Game
             Field<int>("level", x => x.Level);
             Field<int>("job", x => x.Job);
             Field<string>("jobName", x => Enum.GetName(typeof(Job), x.Job));
-            Field<int>("str", x => x.STR);
-            Field<int>("dex", x => x.DEX);
-            Field<int>("int", x => x.INT);
-            Field<int>("luk", x => x.LUK);
-            Field<int>("hp", x => x.HP);
-            Field<int>("maxHp", x => x.MaxHP);
-            Field<int>("mp", x => x.MP);
-            Field<int>("maxMp", x => x.MaxMP);
-            Field<int>("exp", x => x.EXP);
-            Field<int>("pop", x => x.POP);
-            Field<int>("money", x => x.Money);
 
             Field<ServerStateType>("currentServer", resolve: ctx =>
                 service.CharacterStateCache.ExistsAsync(ctx.Source.ID.ToString()).Result
