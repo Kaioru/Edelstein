25bbceabb6cb3f2e12bdb4b875119a85e72732d8
Kaioru <keith@kaioru.co>
2021-05-29 17:53:47 +0800

    Add basic RPC interfaces and protos

---
+++ src/protocol/Edelstein.Protocol.Interop/IAuthenticatorService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Protocol.Interop
+{
+    [ServiceContract]
+    public interface IAuthenticatorService
+    {
+        [OperationContract] Task<AuthenticateResponse> Authenticate(AuthenticateRequest request);
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Interop/ISessionRegistryService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Protocol.Interop
+{
+    [ServiceContract]
+    public interface ISessionRegistryService
+    {
+        [OperationContract] Task<UpdateSessionResult> UpdateSession(UpdateSessionRequest request);
+        [OperationContract] Task<DescribeSessionResult> DescribeSessionByAccount(DescribeSessionByAccountRequest request);
+        [OperationContract] Task<DescribeSessionResult> DescribeSessionByCharacter(DescribeSessionByCharacterRequest request);
+    }
+}
