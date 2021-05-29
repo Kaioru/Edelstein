f2a566b1175b6139a926e437538e67b4d5118f29
Kaioru <keith@kaioru.co>
2021-05-29 19:12:17 +0800

    Add repository and ticker utilities

---
+++ src/protocol/Edelstein.Protocol.Util/Repositories/IRepositoryWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Util.Repositories
+{
+    public interface IRepositoryWriter<
+        TKey,
+        TEntry
+    > where TEntry : class, IRepositoryEntry<TKey>
+    {
+        Task<TEntry> Insert(TEntry entry);
+        Task<IEnumerable<TEntry>> Insert(IEnumerable<TEntry> entries);
+
+        Task Update(TEntry entry);
+
+        Task Delete(TKey key);
+        Task Delete(TEntry entry);
+    }
+}
