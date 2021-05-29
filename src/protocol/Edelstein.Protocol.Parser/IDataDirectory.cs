cf9453c588c5a9a30b92564cd11f883f3a726437
Kaioru <keith@kaioru.co>
2021-05-30 02:35:05 +0800

    Add parser interfaces and NX implementation

---
+++ src/protocol/Edelstein.Protocol.Parser/IDataDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Parser
+{
+    public interface IDataDirectory
+    {
+        string Name { get; }
+        string Path { get; }
+        IEnumerable<IDataProperty> Children { get; }
+
+        IDataProperty Resolve(string path = null);
+        Task<IDataProperty> ResolveAsync(string path = null);
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Parser/IDataDirectoryCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Parser
+{
+    public interface IDataDirectoryCollection
+    {
+        IEnumerable<IDataDirectory> Children { get; }
+
+        IDataProperty Resolve(string path = null);
+        Task<IDataProperty> ResolveAsync(string path = null);
+    }
+}
