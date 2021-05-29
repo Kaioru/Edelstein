219f836b8fe449b52d26795f02e1ddb72f1557ff
Kaioru <keith@kaioru.co>
2021-05-29 21:06:56 +0800

    Add basic gameplay interfaces and templates

---
+++ src/protocol/Edelstein.Protocol.Gameplay/AccountEntity.cs
@@ -0,0 +1,18 @@
+﻿using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay
+{
+    public class AccountEntity : IRepositoryEntry<int>
+    {
+        public int ID { get; init; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PIN { get; set; }
+        public string SPW { get; set; }
+
+        public byte? Gender { get; set; }
+
+        public byte? LatestConnectedWorld { get; set; }
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay/Inventories/Templates/ItemBundleTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace Edelstein.Protocol.Gameplay.Inventories.Templates
+{
+    public record ItemBundleTemplate : ItemTemplate
+    {
+        public double UnitPrice { get; init; }
+        public short MaxPerSlot { get; init; }
+    }
+}
