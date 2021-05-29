e3a281ec3345779c7975a4f78101849154d12993
Kaioru <keith@kaioru.co>
2021-05-29 17:53:47 +0800

    Add basic RPC interfaces and protos

---
+++ src/protocol/Edelstein.Protocol.Interop/IServerRegistryService.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Protocol.Interop
+{
+    [ServiceContract]
+    public interface IServerRegistryService
+    {
+        [OperationContract] Task<ServiceRegistryResponse> RegisterServer(ServiceRegistryRequest request);
+        [OperationContract] Task<ServiceRegistryResponse> DeregisterServer(ServiceRegistryRequest request);
+        [OperationContract] Task<ServiceRegistryResponse> UpdateServer(ServiceRegistryRequest request);
+        [OperationContract] Task<DescribeServerResponse> DescribeServer(DescribeServerRequest request);
+        [OperationContract] Task<DescribeServersResponse> DescribeServers(DescribeServersRequest request);
+
+        [OperationContract] Task<DispatchResponse> Dispatch(DispatchRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToServer(DispatchToServerRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToAlliance(DispatchToAllianceRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToGuild(DispatchToGuildRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToParty(DispatchToPartyRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToCharacter(DispatchToCharacterRequest request);
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Interop/IServerService.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Protocol.Interop
+{
+    [ServiceContract]
+    public interface IServerService
+    {
+        [OperationContract] Task<DispatchResponse> Dispatch(DispatchRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToAlliance(DispatchToAllianceRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToGuild(DispatchToGuildRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToParty(DispatchToPartyRequest request);
+        [OperationContract] Task<DispatchResponse> DispatchToCharacter(DispatchToCharacterRequest request);
+    }
+}
