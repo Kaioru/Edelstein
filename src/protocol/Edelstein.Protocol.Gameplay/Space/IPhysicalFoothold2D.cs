0e2b3631397805a5fb0476619c50b2bbb8a71dd3
Kaioru <keith@kaioru.co>
2021-07-13 17:23:59 +0800

    Move 2d space interfaces to protocol.gameplay

--- src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalFoothold2D.cs
+++ src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalFoothold2D.cs
@@ -1,7 +1,7 @@
 ﻿using Edelstein.Protocol.Util.Repositories;
 using Edelstein.Protocol.Util.Spatial;
 
-namespace Edelstein.Protocol.Gameplay.Stages.Game.Space
+namespace Edelstein.Protocol.Gameplay.Space
 {
     public interface IPhysicalFoothold2D : IRepositoryEntry<int>
     {
