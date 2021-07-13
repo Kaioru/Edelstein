43507aa11a164e958b3d9f283052061b62693d92
Kaioru <keith@kaioru.co>
2021-07-13 19:32:02 +0800

    Add retrieving of all items in physicalspace2d interface

--- src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalSpace2D.cs
+++ src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalSpace2D.cs
@@ -1,4 +1,5 @@
-﻿using Edelstein.Protocol.Util.Spatial;
+﻿using System.Collections.Generic;
+using Edelstein.Protocol.Util.Spatial;
 
 namespace Edelstein.Protocol.Gameplay.Space
 {
@@ -7,11 +8,15 @@ namespace Edelstein.Protocol.Gameplay.Space
         Rect2D Bounds { get; }
 
         IPhysicalPoint2D GetPortal(int id);
-        IPhysicalPoint2D GetPortalClosestTo(Point2D point);
+        ICollection<IPhysicalPoint2D> GetPortals();
+
+        IPhysicalPoint2D GetStartPoint(int id);
         IPhysicalPoint2D GetStartPointClosestTo(Point2D point);
+        ICollection<IPhysicalPoint2D> GetStartPoints();
 
         IPhysicalLine2D GetFoothold(int id);
         IPhysicalLine2D GetFootholdClosestTo(Point2D point);
         IPhysicalLine2D GetFootholdUnderneath(Point2D point);
+        ICollection<IPhysicalLine2D> GetFootholds();
     }
 }
