011415205ecb2fc93d03d7331abc09095b699db6
Kaioru <keith@kaioru.co>
2021-07-13 19:34:03 +0800

    Change to ienumerable

--- src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalSpace2D.cs
+++ src/protocol/Edelstein.Protocol.Gameplay/Space/IPhysicalSpace2D.cs
@@ -8,15 +8,15 @@ namespace Edelstein.Protocol.Gameplay.Space
         Rect2D Bounds { get; }
 
         IPhysicalPoint2D GetPortal(int id);
-        ICollection<IPhysicalPoint2D> GetPortals();
+        IEnumerable<IPhysicalPoint2D> GetPortals();
 
         IPhysicalPoint2D GetStartPoint(int id);
         IPhysicalPoint2D GetStartPointClosestTo(Point2D point);
-        ICollection<IPhysicalPoint2D> GetStartPoints();
+        IEnumerable<IPhysicalPoint2D> GetStartPoints();
 
         IPhysicalLine2D GetFoothold(int id);
         IPhysicalLine2D GetFootholdClosestTo(Point2D point);
         IPhysicalLine2D GetFootholdUnderneath(Point2D point);
-        ICollection<IPhysicalLine2D> GetFootholds();
+        IEnumerable<IPhysicalLine2D> GetFootholds();
     }
 }
