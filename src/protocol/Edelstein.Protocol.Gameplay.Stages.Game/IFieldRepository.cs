ddaaf8206f4d9d447ac75ce6c027241b2a0a03ec
Kaioru <keith@kaioru.co>
2021-07-15 20:04:24 +0800

    Add field repository interface

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/IFieldRepository.cs
@@ -0,0 +1,10 @@
+﻿using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game
+{
+    public interface IFieldRepository
+        : ILocalRepositoryReader<int, IField>
+        , ILocalRepositoryWriter<int, IField>
+    {
+    }
+}
