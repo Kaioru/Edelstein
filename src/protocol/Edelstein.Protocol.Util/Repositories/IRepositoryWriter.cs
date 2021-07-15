54b7127dba612662a34e4d80bb38245a4631ca31
Kaioru <keith@kaioru.co>
2021-07-16 00:26:22 +0800

    Expand repository api bulk capabilities

--- src/protocol/Edelstein.Protocol.Util/Repositories/IRepositoryWriter.cs
+++ src/protocol/Edelstein.Protocol.Util/Repositories/IRepositoryWriter.cs
@@ -12,8 +12,11 @@ namespace Edelstein.Protocol.Util.Repositories
         Task<IEnumerable<TEntry>> Insert(IEnumerable<TEntry> entries);
 
         Task Update(TEntry entry);
+        Task Update(IEnumerable<TEntry> entries);
 
         Task Delete(TKey key);
         Task Delete(TEntry entry);
+        Task Delete(IEnumerable<TKey> keys);
+        Task Delete(IEnumerable<TEntry> entries);
     }
 }
