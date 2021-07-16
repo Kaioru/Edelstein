b3d8fb796ac77da8fce05438e011a61f6cbabacf
Kaioru <keith@kaioru.co>
2021-07-16 09:13:16 +0800

    Add game stage interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/IGameStage.cs
@@ -0,0 +1,29 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Game.FieldSets;
+using Edelstein.Protocol.Gameplay.Stages.Game.Templates;
+using Edelstein.Protocol.Gameplay.Templating;
+using Edelstein.Protocol.Gameplay.Users;
+using Edelstein.Protocol.Gameplay.Users.Inventories.Templates;
+using Edelstein.Protocol.Interop;
+using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game
+{
+    public interface IGameStage : IStage<IGameStage, IGameStageUser>, IRepositoryEntry<int>
+    {
+        int WorldID { get; }
+
+        IServerRegistryService ServerRegistryService { get; }
+        ISessionRegistryService SessionRegistry { get; }
+        IMigrationRegistryService MigrationRegistryService { get; }
+
+        IAccountRepository AccountRepository { get; }
+        IAccountWorldRepository AccountWorldRepository { get; }
+        ICharacterRepository CharacterRepository { get; }
+
+        ITemplateRepository<ItemTemplate> ItemTemplates { get; }
+        ITemplateRepository<FieldTemplate> FieldTemplates { get; }
+
+        IFieldRepository FieldRepository { get; }
+        IFieldSetRepository FieldSetRepository { get; }
+    }
+}
