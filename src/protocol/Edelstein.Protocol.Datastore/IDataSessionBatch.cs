d92f1b7580244151ec79b217f6381170126518b5
Kaioru <keith@kaioru.co>
2021-07-16 13:06:11 +0800

    Add Datastore related interfaces

---
+++ src/protocol/Edelstein.Protocol.Datastore/IDataSessionBatch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Datastore
+{
+    public interface IDataSessionBatch : IDataSession, IDisposable
+    {
+        Task Discard();
+        Task Save();
+    }
+}
