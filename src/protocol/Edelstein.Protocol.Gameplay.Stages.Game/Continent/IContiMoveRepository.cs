42f775f24248e1abd41f81f0a0fe6f1a4593d257
Kaioru <keith@kaioru.co>
2021-07-17 20:55:21 +0800

    Add more capabilities to fieldset and contimove interfaces

--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Continent/IContiMoveRepository.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Continent/IContiMoveRepository.cs
@@ -3,7 +3,7 @@ using Edelstein.Protocol.Util.Repositories;
 
 namespace Edelstein.Protocol.Gameplay.Stages.Game.Continent
 {
-    public interface IContiMoveRepository : ILocalRepositoryReader<int, IContiMove>
+    public interface IContiMoveRepository : ILocalRepository<int, IContiMove>
     {
         Task<IContiMove> RetrieveByInfo(string info);
         Task<IContiMove> RetrieveByField(IField field);
