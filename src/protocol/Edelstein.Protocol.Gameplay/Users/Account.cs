1917e9a5f38a166047104497a27308f1cbbcc75a
Kaioru <keith@kaioru.co>
2021-07-17 21:51:57 +0800

    Add gradecode and subgradecode to account

--- src/protocol/Edelstein.Protocol.Gameplay/Users/Account.cs
+++ src/protocol/Edelstein.Protocol.Gameplay/Users/Account.cs
@@ -12,6 +12,9 @@ namespace Edelstein.Protocol.Gameplay.Users
         public string PIN { get; set; }
         public string SPW { get; set; }
 
+        public AccountGradeCode GradeCode { get; set; }
+        public AccountSubGradeCode SubGradeCode { get; set; }
+
         public byte? Gender { get; set; }
 
         public byte? LatestConnectedWorld { get; set; }
