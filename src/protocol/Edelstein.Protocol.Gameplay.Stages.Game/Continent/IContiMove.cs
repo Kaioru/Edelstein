15514dcb050f6265053eb0593dec7018f049130d
Kaioru <keith@kaioru.co>
2021-07-17 20:16:20 +0800

    Add contimove interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Continent/IContiMove.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Stages.Game.Continent.Templates;
+using Edelstein.Protocol.Network;
+using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Continent
+{
+    public interface IContiMove : IPacketDispatcher, IRepositoryEntry<int>
+    {
+        ContiMoveTemplate Template { get; }
+
+        ContiMoveState State { get; }
+
+        DateTime NextBoarding { get; }
+        DateTime NextEvent { get; }
+        bool IsEventActive { get; }
+
+        IField StartShipMoveField { get; }
+        IField WaitField { get; }
+        IField MoveField { get; }
+        IField CabinField { get; }
+        IField EndField { get; }
+        IField EndShipMoveField { get; }
+
+        Task Trigger(ContiMoveStateTrigger trigger);
+        Task Dispatch(ContiMoveTarget target, IPacket packet);
+    }
+}
