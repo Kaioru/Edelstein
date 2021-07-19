5e9a19d9c285efd2ccf3c9a5a56b9879790405e4
Kaioru <keith@kaioru.co>
2021-07-19 20:40:05 +0800

    Add NLua implementation for Protocol.Scripting

---
+++ src/common/Edelstein.Common.Scripting.NLua/LuaScriptEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Scripting;
+
+namespace Edelstein.Common.Scripting.NLua
+{
+    public class LuaScriptEngine : IScriptEngine
+    {
+        public Task<object> Evaluate(string source, IScriptScope scope = null)
+            => new LuaScript(source).Evaluate(scope);
+
+        public Task<object> EvaluateFromFile(string path, IScriptScope scope = null)
+            => new LuaScript(File.ReadAllText(path)).Evaluate(scope);
+
+        public Task<IScript> Create(string source)
+            => Task.FromResult<IScript>(new LuaScript(source));
+
+        public Task<IScript> CreateFromFile(string path)
+            => Task.FromResult<IScript>(new LuaScript(File.ReadAllText(path)));
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Scripting/IScriptScope.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Edelstein.Protocol.Scripting
+{
+    public interface IScriptScope
+    {
+        IDictionary<string, object> Globals { get; }
+
+        void Register(string key, object value);
+        void Deregister(string key);
+    }
+}
