46a56bf99554f117d9b4f395a2b31ed8d6fc3ed2
Kaioru <keith@kaioru.co>
2021-07-20 17:47:29 +0800

    Change user repositories to use cachedrepository

--- src/common/Edelstein.Common.Gameplay/Users/CharacterRepository.cs
+++ src/common/Edelstein.Common.Gameplay/Users/CharacterRepository.cs
@@ -1,11 +1,27 @@
-﻿using Edelstein.Common.Util.Repositories;
+﻿using System;
+using Edelstein.Common.Util.Caching;
+using Edelstein.Common.Util.Repositories;
 using Edelstein.Protocol.Datastore;
 using Edelstein.Protocol.Gameplay.Users;
+using Edelstein.Protocol.Util.Caching;
 
 namespace Edelstein.Common.Gameplay.Users
 {
-    public class CharacterRepository : DataStoreRepository<Character>, ICharacterRepository
+    public class CharacterRepository : CachedRepository<int, Character>, ICharacterRepository
     {
-        public CharacterRepository(IDataStore store) : base(store) { }
+        public static readonly string CacheScope = "characters";
+        public static readonly TimeSpan CacheDuration = TimeSpan.FromHours(3);
+
+        private readonly IDataStore _store;
+
+        public CharacterRepository(
+            ICache cache,
+            IDataStore store
+        ) : base(
+            new ScopedCache(CacheScope, cache),
+            new DataStoreRepository<Character>(store),
+            CacheDuration
+        )
+        => _store = store;
     }
 }
