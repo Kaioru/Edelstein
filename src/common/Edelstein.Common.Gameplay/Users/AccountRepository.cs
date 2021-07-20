1bf773ac332e6d20d3324821bf0eb8ef601f8625
Kaioru <keith@kaioru.co>
2021-07-20 17:47:29 +0800

    Change user repositories to use cachedrepository

--- src/common/Edelstein.Common.Gameplay/Users/AccountRepository.cs
+++ src/common/Edelstein.Common.Gameplay/Users/AccountRepository.cs
@@ -1,17 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Common.Util.Caching;
 using Edelstein.Common.Util.Repositories;
 using Edelstein.Protocol.Datastore;
 using Edelstein.Protocol.Gameplay.Users;
+using Edelstein.Protocol.Util.Caching;
 
 namespace Edelstein.Common.Gameplay.Users
 {
-    public class AccountRepository : DataStoreRepository<Account>, IAccountRepository
+    public class AccountRepository : CachedRepository<int, Account>, IAccountRepository
     {
-        public AccountRepository(IDataStore store) : base(store) { }
+        public static readonly string CacheScope = "accounts";
+        public static readonly TimeSpan CacheDuration = TimeSpan.FromHours(3);
+
+        private readonly IDataStore _store;
+
+        public AccountRepository(
+            ICache cache,
+            IDataStore store
+        ) : base(
+            new ScopedCache(CacheScope, cache),
+            new DataStoreRepository<Account>(store),
+            CacheDuration
+        )
+        => _store = store;
 
         public Task<Account> RetrieveByUsername(string username)
         {
-            using var session = Store.StartSession();
+            using var session = _store.StartSession();
             return session
                 .Query<Account>()
                 .Where(a => a.Username == username)
--- src/common/Edelstein.Common.Gameplay/Users/AccountWorldRepository.cs
+++ src/common/Edelstein.Common.Gameplay/Users/AccountWorldRepository.cs
@@ -1,17 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Common.Util.Caching;
 using Edelstein.Common.Util.Repositories;
 using Edelstein.Protocol.Datastore;
 using Edelstein.Protocol.Gameplay.Users;
+using Edelstein.Protocol.Util.Caching;
 
 namespace Edelstein.Common.Gameplay.Users
 {
-    public class AccountWorldRepository : DataStoreRepository<AccountWorld>, IAccountWorldRepository
+    public class AccountWorldRepository : CachedRepository<int, AccountWorld>, IAccountWorldRepository
     {
-        public AccountWorldRepository(IDataStore store) : base(store) { }
+        public static readonly string CacheScope = "accountworlds";
+        public static readonly TimeSpan CacheDuration = TimeSpan.FromHours(3);
+
+        private readonly IDataStore _store;
+
+        public AccountWorldRepository(
+            ICache cache,
+            IDataStore store
+        ) : base(
+            new ScopedCache(CacheScope, cache),
+            new DataStoreRepository<AccountWorld>(store),
+            CacheDuration
+        )
+        => _store = store;
 
         public Task<AccountWorld> RetrieveByAccountAndWorld(int account, int world)
         {
-            using var session = Store.StartSession();
+            using var session = _store.StartSession();
             return session
                 .Query<AccountWorld>()
                 .Where(a => a.AccountID == account && a.WorldID == world)
