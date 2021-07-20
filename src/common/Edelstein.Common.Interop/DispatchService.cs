1700b029b8f64ab779b5b7f1d627ea624f514244
Kaioru <keith@kaioru.co>
2021-07-20 12:08:43 +0800

    Add incomplete implementations of interop

---
+++ src/common/Edelstein.Common.Interop/DispatchService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Stages;
+using Edelstein.Protocol.Interop;
+using Edelstein.Protocol.Interop.Contracts;
+using Edelstein.Protocol.Network;
+
+namespace Edelstein.Common.Interop
+{
+    public class DispatchService<TStage, TUser> : IDispatchService
+        where TStage : IStage<TStage, TUser>
+        where TUser : IStageUser<TStage, TUser>
+    {
+        private readonly TStage _stage;
+
+        public DispatchService(TStage stage)
+            => _stage = stage;
+
+        public async Task<DispatchResponse> Dispatch(DispatchRequest request)
+        {
+            var packet = new UnstructuredOutgoingPacket();
+            var buffer = request.Packet.ToByteArray();
+            var targets = _stage.Users.ToList();
+
+            packet.WriteBytes(buffer);
+            await Task.WhenAll(targets.Select(target => target.Dispatch(packet)));
+
+            return new DispatchResponse
+            {
+                Result = DispatchResult.Ok,
+                Reach = targets.Count
+            };
+        }
+
+        public Task<DispatchResponse> DispatchToAlliance(DispatchToAllianceRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToGuild(DispatchToGuildRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToParty(DispatchToPartyRequest request) { throw new NotImplementedException(); }
+
+        public async Task<DispatchResponse> DispatchToCharacter(DispatchToCharacterRequest request)
+        {
+            var target = _stage.Users.FirstOrDefault(u => u.Character.ID == request.Character);
+
+            if (target != null)
+            {
+                var packet = new UnstructuredOutgoingPacket();
+                var buffer = request.Packet.ToByteArray();
+
+                packet.WriteBytes(buffer);
+                await target.Dispatch(packet);
+                return new DispatchResponse
+                {
+                    Result = DispatchResult.Ok,
+                    Reach = 1
+                };
+            }
+
+            return new DispatchResponse
+            {
+                Result = DispatchResult.Failed,
+                Reach = 0
+            };
+        }
+    }
+}
---
+++ src/common/Edelstein.Common.Interop/ServerRegistryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Common.Interop
+{
+    public class ServerRegistryService : IServerRegistryService
+    {
+        public Task<ServiceRegistryResponse> RegisterServer(RegisterServerRequest request) { throw new NotImplementedException(); }
+        public Task<ServiceRegistryResponse> DeregisterServer(DeregisterServerRequest request) { throw new NotImplementedException(); }
+        public Task<ServiceRegistryResponse> UpdateServer(UpdateServerRequest request) { throw new NotImplementedException(); }
+        public Task<DescribeServerResponse> DescribeServer(DescribeServerRequest request) { throw new NotImplementedException(); }
+        public Task<DescribeServersResponse> DescribeServers(DescribeServersRequest request) { throw new NotImplementedException(); }
+
+        public Task<DispatchResponse> Dispatch(DispatchRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToServer(DispatchToServerRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToServers(DispatchToServersRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToAlliance(DispatchToAllianceRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToGuild(DispatchToGuildRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToParty(DispatchToPartyRequest request) { throw new NotImplementedException(); }
+        public Task<DispatchResponse> DispatchToCharacter(DispatchToCharacterRequest request) { throw new NotImplementedException(); }
+    }
+}
