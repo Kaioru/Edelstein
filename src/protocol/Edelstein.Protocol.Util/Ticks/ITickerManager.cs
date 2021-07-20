9b1a34c7e602c76b040354712ffdb37c0ac049b3
Kaioru <keith@kaioru.co>
2021-07-20 09:11:30 +0800

    Add delay param to ticketmanager

--- src/protocol/Edelstein.Protocol.Util/Ticks/ITickerManager.cs
+++ src/protocol/Edelstein.Protocol.Util/Ticks/ITickerManager.cs
@@ -5,7 +5,8 @@ namespace Edelstein.Protocol.Util.Ticks
 {
     public interface ITickerManager : ITicker
     {
-        Task<ITickerManagerEntry> Schedule(ITickerBehavior behavior, TimeSpan time);
+        Task<ITickerManagerEntry> Schedule(ITickerBehavior behavior, TimeSpan frequency);
+        Task<ITickerManagerEntry> Schedule(ITickerBehavior behavior, TimeSpan frequency, TimeSpan delay);
         Task<ITickerManagerEntry> Execute(ITickerBehavior behavior);
     }
 }
