025bc28727c55e305341fb93765ebad591b7997e
Kaioru <keith@kaioru.co>
2021-07-20 16:21:08 +0800

    Add caching interfaces and implementations

---
+++ src/protocol/Edelstein.Protocol.Caching/Serializer/ICacheSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Caching.Serializer
+{
+    public interface ICacheSerializer
+    {
+        Task<T> Deserialize<T>(string data);
+        Task<string> Serialize<T>(T obj);
+    }
+}
