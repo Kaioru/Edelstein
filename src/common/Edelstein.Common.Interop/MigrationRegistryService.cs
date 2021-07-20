2d9c6c3539f97faaedcb2d351e3ef98a548c19ce
Kaioru <keith@kaioru.co>
2021-07-20 12:08:43 +0800

    Add incomplete implementations of interop

---
+++ src/common/Edelstein.Common.Interop/MigrationRegistryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Common.Interop
+{
+    public class MigrationRegistryService : IMigrationRegistryService
+    {
+        public Task<MigrationRegistryResponse> Register(RegisterMigrationRequest request) { throw new NotImplementedException(); }
+        public Task<MigrationRegistryResponse> Deregister(DeregisterMigrationRequest request) { throw new NotImplementedException(); }
+        public Task<ClaimMigrationResponse> Claim(ClaimMigrationRequest request) { throw new NotImplementedException(); }
+    }
+}
---
+++ src/common/Edelstein.Common.Interop/SessionRegistryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Interop;
+using Edelstein.Protocol.Interop.Contracts;
+
+namespace Edelstein.Common.Interop
+{
+    public class SessionRegistryService : ISessionRegistryService
+    {
+        public Task<UpdateSessionResult> UpdateSession(UpdateSessionRequest request) { throw new NotImplementedException(); }
+        public Task<DescribeSessionResult> DescribeSessionByAccount(DescribeSessionByAccountRequest request) { throw new NotImplementedException(); }
+        public Task<DescribeSessionResult> DescribeSessionByCharacter(DescribeSessionByCharacterRequest request) { throw new NotImplementedException(); }
+    }
+}
