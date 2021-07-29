1727dbfd96ffc977a83e0c5ff19a3fb7e37ca9c4
Kaioru <keith@kaioru.co>
2021-07-29 21:28:10 +0800

    Add ticker and tickermanager implementations

--- src/app/Playground/Program.cs
+++ src/app/Playground/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using Edelstein.Common.Gamplay.Stages.Game.Templates;
 using Edelstein.Common.Parser.Duey;
+using Edelstein.Common.Util.Ticks;
+using Edelstein.Protocol.Util.Ticks;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
@@ -29,6 +32,17 @@ namespace Playground
 
             Console.WriteLine($"Edelstein is {fieldTemplate.Bounds.Size.Width}x{fieldTemplate.Bounds.Size.Height} big");
             Console.WriteLine($"Took {stopwatch.Elapsed} to retrieve");
+
+            var manager = new TickerManager(4, loggerFactory.CreateLogger<ITicker>());
+            var token = new CancellationTokenSource();
+
+            manager.Execute(new TestingTickerBehavior(), TimeSpan.FromSeconds(1));
+
+            Console.CancelKeyPress += delegate { token.Cancel(); };
+
+            while (!token.IsCancellationRequested)
+            {
+            }
         }
     }
 }
