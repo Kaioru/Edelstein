1f9262ea540eb1244217060a43ea400c0086b0ca
Kaioru <keith@kaioru.co>
2021-07-30 20:14:15 +0800

    Add session initializers

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/GameSessionInitializer.cs
@@ -0,0 +1,10 @@
+﻿using Edelstein.Protocol.Network.Session;
+using Edelstein.Protocol.Network.Transport;
+
+namespace Edelstein.Common.Gameplay.Stages.Game
+{
+    public class GameSessionInitializer : ISessionInitializer
+    {
+        public ISession Initialize(ISocket socket) => new GameStageUser(socket);
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/LoginSessionInitializer.cs
@@ -0,0 +1,10 @@
+﻿using Edelstein.Protocol.Network.Session;
+using Edelstein.Protocol.Network.Transport;
+
+namespace Edelstein.Common.Gameplay.Stages.Login
+{
+    public class LoginSessionInitializer : ISessionInitializer
+    {
+        public ISession Initialize(ISocket socket) => new LoginStageUser(socket);
+    }
+}
