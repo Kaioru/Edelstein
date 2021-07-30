b295eca76f2359b5567bc1713937da419eb3d038
Kaioru <keith@kaioru.co>
2021-07-30 21:54:16 +0800

    Add movement interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Movements/IMovePath.cs
@@ -0,0 +1,15 @@
+﻿using Edelstein.Protocol.Network;
+using Edelstein.Protocol.Util.Spatial;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Movements
+{
+    public interface IMovePath
+    {
+        MoveActionType? Type { get; }
+        Point2D? Position { get; }
+        short? Foothold { get; }
+
+        void Decode(IPacketReader reader);
+        void Encode(IPacketWriter writer);
+    }
+}
