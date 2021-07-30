35989035d3d9acf7d7e4c97924432f3dede67b0b
Kaioru <keith@kaioru.co>
2021-07-30 21:17:28 +0800

    Add character field and portal record updating

--- src/common/Edelstein.Common.Gameplay.Stages.Game/GameStageUser.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/GameStageUser.cs
@@ -1,4 +1,5 @@
-﻿using Edelstein.Protocol.Gameplay.Stages.Game;
+﻿using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Stages.Game;
 using Edelstein.Protocol.Gameplay.Stages.Game.Objects;
 using Edelstein.Protocol.Network.Transport;
 
@@ -10,5 +11,16 @@ namespace Edelstein.Common.Gameplay.Stages.Game
         public IFieldObjUser FieldUser { get; set; }
 
         public GameStageUser(ISocket socket) : base(socket) { }
+
+        public override async Task OnDisconnect()
+        {
+            if (Character != null && Field != null && FieldUser != null)
+            {
+                Character.FieldID = Field.Template.FieldReturn ?? Field.ID;
+                Character.FieldPortal = (byte)Field.GetStartPointClosestTo(FieldUser.Position).ID;
+            }
+
+            await base.OnDisconnect();
+        }
     }
 }
