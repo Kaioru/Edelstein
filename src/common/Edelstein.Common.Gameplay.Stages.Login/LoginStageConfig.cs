1b5b5b69d6fb477b0276726cc6839011e737077c
Kaioru <keith@kaioru.co>
2021-08-05 21:20:39 +0800

    Add worldrequest and checkuserlimit handlers

--- src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStageConfig.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStageConfig.cs
@@ -3,6 +3,6 @@
     public record LoginStageConfig : ServerStageConfig
     {
         public bool AutoRegister { get; init; } = false;
-        public byte[] Worlds { get; init; } = { 0 };
+        public byte[] Worlds { get; init; }
     }
 }
--- src/common/Edelstein.Common.Gameplay.Stages.Login/Templates/WorldTemplate.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Templates/WorldTemplate.cs
@@ -8,6 +8,7 @@ namespace Edelstein.Common.Gameplay.Stages.Login.Templates
         public int ID { get; init; }
         public string Name { get; init; }
         public byte State { get; init; }
+        public bool BlockCharCreation { get; init; }
 
         public WorldTemplate(int id, IDataProperty property)
         {
@@ -15,6 +16,7 @@ namespace Edelstein.Common.Gameplay.Stages.Login.Templates
 
             Name = property.ResolveOrDefault<string>("name") ?? "NO-NAME";
             State = property.Resolve<byte>("state") ?? 0;
+            BlockCharCreation = property.Resolve<bool>("blockCharCreation") ?? false;
         }
     }
 }
