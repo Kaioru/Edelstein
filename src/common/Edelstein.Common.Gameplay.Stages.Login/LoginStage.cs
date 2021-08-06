ed280d65f637b011a328e424723ad2ca3da31e88
Kaioru <keith@kaioru.co>
2021-08-06 15:56:11 +0800

    Implement EnableSPWRequest handler

--- src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStage.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStage.cs
@@ -59,6 +59,7 @@ namespace Edelstein.Common.Gameplay.Stages.Login
             processor.Register(new LogoutWorldHandler());
             processor.Register(new CheckDuplicatedIDHandler());
             processor.Register(new CreateNewCharacterHandler());
+            processor.Register(new EnableSPWRequestHandler(false));
         }
 
         public override async Task Enter(LoginStageUser user)
