71d885d6b8e2c2c6c2a634986a747328782cfc96
Kaioru <keith@kaioru.co>
2021-08-06 10:24:56 +0800

    Add world info request handler (used when going back from character select)

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/WorldInfoRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Edelstein.Common.Gameplay.Handling;
+using Edelstein.Protocol.Network;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Handlers
+{
+    public class WorldInfoRequestHandler : MirroredPacketHandler<LoginStage, LoginStageUser>
+    {
+        public WorldInfoRequestHandler() : base(
+            (short)PacketRecvOperations.WorldInfoRequest,
+            new WorldRequestHandler()
+        )
+        {
+        }
+
+        public override Task<bool> Check(LoginStageUser user)
+            => Task.FromResult(user.State == LoginState.SelectCharacter);
+
+        public override Task Handle(LoginStageUser user, IPacketReader packet)
+        {
+            user.State = LoginState.SelectWorld;
+            user.SelectedWorldID = null;
+            user.SelectedChannelID = null;
+
+            return base.Handle(user, packet);
+        }
+    }
+}
