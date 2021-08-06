e0110315d8ae48b4109ec860aa55e58b2dfa7521
Kaioru <keith@kaioru.co>
2021-08-06 12:40:58 +0800

    Implement modify inventory operations

---
+++ src/common/Edelstein.Common.Gameplay/Users/Inventories/Modify/AbstractModifyInventoryOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Edelstein.Protocol.Gameplay.Users.Inventories;
+using Edelstein.Protocol.Gameplay.Users.Inventories.Modify;
+using Edelstein.Protocol.Network;
+
+namespace Edelstein.Common.Gameplay.Users.Inventories.Modify
+{
+    public abstract class AbstractModifyInventoryOperation : IModifyInventoryOperation
+    {
+        public abstract ModifyInventoryOperations Operation { get; }
+
+        public ItemInventoryType Type { get; init; }
+        public short Slot { get; init; }
+        public DateTime Timestamp { get; init; }
+
+        public AbstractModifyInventoryOperation()
+            => Timestamp = DateTime.UtcNow;
+
+        public void WriteBase(IPacketWriter writer)
+        {
+            writer.WriteByte((byte)Operation);
+            writer.WriteByte((byte)Type);
+            writer.WriteShort(Slot);
+
+            WriteData(writer);
+        }
+
+        public virtual void WriteData(IPacketWriter writer) { }
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay/Users/Inventories/Modify/Operations/UpdateQuantityModifyInventoryOperation.cs
@@ -0,0 +1,27 @@
+﻿using Edelstein.Protocol.Gameplay.Users.Inventories;
+using Edelstein.Protocol.Gameplay.Users.Inventories.Modify;
+using Edelstein.Protocol.Network;
+
+namespace Edelstein.Common.Gameplay.Users.Inventories.Modify.Operations
+{
+    public class UpdateQuantityModifyInventoryOperation : AbstractModifyInventoryOperation
+    {
+        public override ModifyInventoryOperations Operation => ModifyInventoryOperations.UpdateQuantity;
+
+        public short Quantity { get; init; }
+
+        public UpdateQuantityModifyInventoryOperation(
+            ItemInventoryType type,
+            short slot,
+            short quantity
+        )
+        {
+            Type = type;
+            Slot = slot;
+            Quantity = quantity;
+        }
+
+        public override void WriteData(IPacketWriter writer)
+            => writer.WriteShort(Quantity);
+    }
+}
