8259f03184308ce845e8a937281da9a31cd08034
Kaioru <keith@kaioru.co>
2021-08-13 20:52:12 +0800

    Add calculated rates and stats interface

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/ICalculatedRates.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats
+{
+    public interface ICalculatedRates
+    {
+        public int Exp { get; }
+        public int Reward { get; }
+        public int Money { get; }
+
+        Task Calculate();
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/ICalculatedStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats
+{
+    public interface ICalculatedStats
+    {
+        public int STR { get; }
+        public int DEX { get; }
+        public int INT { get; }
+        public int LUK { get; }
+
+        public int MaxHP { get; }
+        public int MaxMP { get; }
+
+        public int PAD { get; }
+        public int PDD { get; }
+        public int MAD { get; }
+        public int MDD { get; }
+        public int ACC { get; }
+        public int EVA { get; }
+        public int Craft { get; }
+        public int Speed { get; }
+        public int Jump { get; }
+
+        public int STRr { get; }
+        public int DEXr { get; }
+        public int INTr { get; }
+        public int LUKr { get; }
+
+        public int MaxHPr { get; }
+        public int MaxMPr { get; }
+
+        public int PADr { get; }
+        public int PDDr { get; }
+        public int MADr { get; }
+        public int MDDr { get; }
+        public int ACCr { get; }
+        public int EVAr { get; }
+
+        Task Calculate();
+    }
+}
