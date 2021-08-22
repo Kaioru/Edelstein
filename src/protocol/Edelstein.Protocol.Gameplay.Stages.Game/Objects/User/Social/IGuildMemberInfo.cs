7957ed7ed40f429d1035f09cd0065582e1a0dd35
Kaioru <keith@kaioru.co>
2021-08-22 15:08:18 +0800

    Add guild/party info interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Social/IGuildMemberInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Social
+{
+    public interface IGuildMemberInfo
+    {
+        int CharacterID { get; }
+        string CharacterName { get; }
+
+        int Job { get; }
+        int Level { get; }
+        int Grade { get; }
+        bool Online { get; }
+
+        int Commitment { get; }
+        bool Inactive { get; }
+    }
+}
