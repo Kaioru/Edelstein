5c19c0f09cadfd3bf937da1d414afebda939566f
Kaioru <keith@kaioru.co>
2021-08-24 21:55:22 +0800

    Add basic protos for party and guild

--- src/protocol/Edelstein.Protocol.Services/Social/IGuildService.cs
+++ src/protocol/Edelstein.Protocol.Services/Social/IGuildService.cs
@@ -1,6 +1,17 @@
-﻿namespace Edelstein.Protocol.Services.Social
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Edelstein.Protocol.Services.Contracts.Social;
+using ProtoBuf.Grpc;
+
+namespace Edelstein.Protocol.Services.Social
 {
+    [ServiceContract]
     public interface IGuildService
     {
+        [OperationContract] Task<GuildLoadByIDResponse> LoadByID(GuildLoadByIDRequest request);
+        [OperationContract] Task<GuildLoadByCharacterResponse> LoadByCharacter(GuildLoadByCharacterRequest request);
+
+        [OperationContract] IAsyncEnumerable<GuildUpdateContract> Subscribe(CallContext context = default);
     }
 }
