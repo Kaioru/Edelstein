bef4c6cbbf4223973197dc1734f6a1f61465f7ab
Kaioru <keith@kaioru.co>
2021-08-30 20:52:49 +0800

    Add mob implementation and mob spawning

--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Generators/IFieldGenerator.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Generators/IFieldGenerator.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Edelstein.Protocol.Gameplay.Stages.Game.Generators
 {
-    public interface IFieldGenerator
+    public interface AbstractFieldMobGenerator
     {
         bool Check(DateTime now, IField field);
         Task Generate(IField field);
