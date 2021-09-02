e159dc59d43e0b6d8488005087ea086802bd1bde
Kaioru <keith@kaioru.co>
2021-09-02 21:32:18 +0800

    Add Edelstein.App.Game

--- src/app/Edelstein.App.Game/Program.cs
+++ src/app/Edelstein.App.Game/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System.Threading.Tasks;
 using Edelstein.Common.Gameplay.Handling;
-using Edelstein.Common.Gameplay.Stages.Login;
-using Edelstein.Common.Gameplay.Stages.Login.Templates;
+using Edelstein.Common.Gameplay.Stages.Game;
+using Edelstein.Common.Gameplay.Stages.Game.Commands;
+using Edelstein.Common.Gameplay.Stages.Game.Continent.Templates;
+using Edelstein.Common.Gameplay.Stages.Game.Objects.Mob.Templates;
+using Edelstein.Common.Gameplay.Stages.Game.Objects.NPC.Templates;
+using Edelstein.Common.Gameplay.Stages.Game.Templates;
 using Edelstein.Common.Gameplay.Templating;
 using Edelstein.Common.Gameplay.Users;
 using Edelstein.Common.Gameplay.Users.Inventories.Templates;
+using Edelstein.Common.Gameplay.Users.Inventories.Templates.Options;
+using Edelstein.Common.Gameplay.Users.Inventories.Templates.Sets;
 using Edelstein.Common.Hosting;
 using Edelstein.Common.Network.DotNetty;
 using Edelstein.Common.Services;
 using Edelstein.Common.Services.Social;
 using Edelstein.Common.Util.Ticks;
+using Edelstein.Protocol.Gameplay.Stages.Game.Commands;
 using Edelstein.Protocol.Gameplay.Users;
 using Edelstein.Protocol.Network.Session;
 using Edelstein.Protocol.Services;
@@ -21,7 +28,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
-namespace Edelstein.App.Login
+namespace Edelstein.App.Game
 {
     internal static class Program
     {
@@ -69,19 +76,27 @@ namespace Edelstein.App.Login
 
                     builder.AddSingleton<ITickerManager, TickerManager>();
 
-                    builder.AddSingleton<ITemplateRepository<WorldTemplate>, WorldTemplateRepository>();
                     builder.AddSingleton<ITemplateRepository<ItemTemplate>, ItemTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<ItemStringTemplate>, ItemStringTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<ItemOptionTemplate>, ItemOptionTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<ItemSetTemplate>, ItemSetTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<FieldTemplate>, FieldTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<FieldStringTemplate>, FieldStringTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<ContiMoveTemplate>, ContiMoveTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<NPCTemplate>, NPCTemplateRepository>();
+                    builder.AddSingleton<ITemplateRepository<MobTemplate>, MobTemplateRepository>();
 
-                    builder.AddSingleton<ISessionInitializer, LoginSessionInitializer>();
+                    builder.AddSingleton<ISessionInitializer, GameSessionInitializer>();
                     builder.AddNettyAcceptor(config.Version, config.Patch, config.Locale);
 
                     builder.AddSingleton<
-                        IPacketProcessor<LoginStage, LoginStageUser>,
-                        PacketProcessor<LoginStage, LoginStageUser>
+                        IPacketProcessor<GameStage, GameStageUser>,
+                        PacketProcessor<GameStage, GameStageUser>
                     >();
+                    builder.AddSingleton<ICommandProcessor, CommandProcessor>();
 
-                    builder.AddSingleton<LoginStageConfig>(config);
-                    builder.AddSingleton<LoginStage>();
+                    builder.AddSingleton<GameStageConfig>(config);
+                    builder.AddSingleton<GameStage>();
                     builder.AddHostedService<ProgramHost>();
                 })
                 .RunConsoleAsync();
---
+++ src/app/Edelstein.App.Game/ProgramConfig.cs
@@ -0,0 +1,11 @@
+﻿using Edelstein.Common.Gameplay.Stages.Game;
+
+namespace Edelstein.App.Game
+{
+    public record ProgramConfig : GameStageConfig
+    {
+        public short Version { get; init; } = 95;
+        public string Patch { get; init; } = "1";
+        public byte Locale { get; init; } = 8;
+    }
+}
