8aba35d1aa1f0321cebc936aef93edd3d1c0f6c6
Kaioru <keith@kaioru.co>
2021-09-02 21:32:18 +0800

    Add Edelstein.App.Game

---
+++ src/app/Edelstein.App.Game/ProgramHost.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Edelstein.Common.Gameplay.Stages.Game;
+using Edelstein.Protocol.Network.Transport;
+using Microsoft.Extensions.Hosting;
+
+namespace Edelstein.App.Game
+{
+    public class ProgramHost : IHostedService
+    {
+        private readonly GameStage _stage;
+        private readonly ITransportAcceptor _acceptor;
+
+        public ProgramHost(GameStage stage, ITransportAcceptor acceptor)
+        {
+            _stage = stage;
+            _acceptor = acceptor;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+            => _acceptor.Accept(_stage.Info.Host, _stage.Info.Port);
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => _acceptor.Close();
+    }
+}
