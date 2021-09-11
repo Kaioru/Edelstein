37bd479792eb0d2b4b0b800e33a08538ef9f41f3
Kaioru <keith@kaioru.co>
2021-09-11 11:36:13 +0800

    Add character skill templates and parsing

---
+++ src/common/Edelstein.Common.Gameplay/Users/Skills/Templates/CharacterSkillTemplate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Edelstein.Common.Gameplay.Templating;
+using Edelstein.Protocol.Parser;
+
+namespace Edelstein.Common.Gameplay.Users.Skills.Templates
+{
+    public record CharacterSkillTemplate : ITemplate
+    {
+        public int ID { get; }
+
+        public short MaxLevel { get; }
+
+        public IDictionary<int, int> ReqSkill { get; }
+        public IDictionary<int, CharacterSkillLevelTemplate> LevelData { get; }
+
+        public CharacterSkillTemplate(int id, IDataProperty property)
+        {
+            ID = id;
+
+            var entry = property.Resolve("common");
+            ImmutableDictionary<int, CharacterSkillLevelTemplate> levelData;
+
+            if (entry != null)
+            {
+                var maxLevel = entry.Resolve<int>("maxLevel") ?? 0;
+
+                levelData = Enumerable
+                    .Range(1, maxLevel)
+                    .ToImmutableDictionary(
+                        i => i,
+                        i => new CharacterSkillLevelTemplate(i, entry.ResolveAll())
+                    );
+            }
+            else
+            {
+                entry = property.Resolve("level");
+                levelData = entry.Children.ToImmutableDictionary(
+                    c => Convert.ToInt32(c.Name),
+                    c => new CharacterSkillLevelTemplate(Convert.ToInt32(c.Name), c.ResolveAll(), false)
+                );
+            }
+
+            MaxLevel = (short)levelData.Count;
+
+            ReqSkill = property.Resolve("req")?.Children
+                           .ToImmutableDictionary(
+                               c => Convert.ToInt32(c.Name),
+                               c => c.Resolve<int>() ?? 0
+                           ) ?? ImmutableDictionary<int, int>.Empty;
+            LevelData = levelData;
+        }
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay/Users/Skills/Templates/CharacterSkillTemplateRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Edelstein.Common.Gameplay.Templating;
+using Edelstein.Protocol.Parser;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using MoreLinq;
+
+namespace Edelstein.Common.Gameplay.Users.Skills.Templates
+{
+    public class CharacterSkillTemplateRepository : TemplateRepository<CharacterSkillTemplate>
+    {
+        private static readonly TimeSpan CacheDuration = TimeSpan.FromHours(1);
+
+        public CharacterSkillTemplateRepository(
+            IDataDirectoryCollection collection,
+            ILogger<CharacterSkillTemplateRepository> logger = null
+        ) : base(CacheDuration)
+        {
+            logger ??= new NullLogger<CharacterSkillTemplateRepository>();
+
+            var dirSkills = collection.Resolve("Skill").ResolveAll();
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            dirSkills.Children
+                .Where(c => c.Name.Split(".")[0].All(char.IsDigit))
+                .SelectMany(c => c.Resolve("skill").Children)
+                .Select(n =>
+                {
+                    var id = Convert.ToInt32(n.Name);
+                    return new TemplateProvider<CharacterSkillTemplate>(
+                        id,
+                        () => new CharacterSkillTemplate(
+                            id,
+                            n.ResolveAll()
+                        )
+                    );
+                })
+                .DistinctBy(t => t.ID)
+                .ForEach(t => Register(t));
+
+            logger.LogInformation($"Loaded {Count} character skill templates in {stopwatch.Elapsed}");
+
+            stopwatch.Stop();
+        }
+    }
+}
