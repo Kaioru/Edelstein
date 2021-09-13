84441aeffa31de7e29b7869182721f1fcd929db8
Kaioru <keith@kaioru.co>
2021-09-13 22:30:17 +0800

    Add guidedbullet and partybooster stat getters

--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/ISecondaryStats.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/ISecondaryStats.cs
@@ -7,7 +7,10 @@ namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats
     {
         IEnumerable<ITemporaryStat> TemporaryStats { get; }
 
-        int GetStatOption(SecondaryStatType type);
-        bool HasStatOption(SecondaryStatType type);
+        bool HasStat(SecondaryStatType type);
+
+        int GetStat(SecondaryStatType type);
+        ITwoStateGuidedBulletStat GetGuidedBulletStat();
+        ITwoStatePartyBoosterStat GetPartyBoosterStat();
     }
 }
