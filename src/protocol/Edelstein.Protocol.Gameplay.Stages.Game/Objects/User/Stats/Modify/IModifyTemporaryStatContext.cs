4c8b56cb92e6a753e80251f354baf5e6b9013227
Kaioru <keith@kaioru.co>
2021-09-13 21:36:13 +0800

    Rework twostatetemporarystat interfaces

--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/Modify/IModifyTemporaryStatContext.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/Modify/IModifyTemporaryStatContext.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats.Temporary;
 
 namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats.Modify
 {
     public interface IModifyTemporaryStatContext
     {
+        void Set(ITemporaryStat stat);
         void Set(
             SecondaryStatType type,
             int value,
