31e2beb516ff0fa0583541ef51036abb4b6ce6f6
Kaioru <keith@kaioru.co>
2021-09-13 21:12:17 +0800

    Add secondarystat, temporarystat interfaces to protocol

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/Temporary/ITwoStateGuidedBulletStat.cs
@@ -0,0 +1,8 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats.Temporary
+{
+    public interface ITwoStateGuidedBulletStat : ITwoStateTemporaryStat
+    {
+        int MobID { get; }
+        int UserID { get; }
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/User/Stats/Temporary/ITwoStateTemporaryStat.cs
@@ -0,0 +1,7 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Stats.Temporary
+{
+    public interface ITwoStateTemporaryStat : ITemporaryStat
+    {
+        void Reset();
+    }
+}
