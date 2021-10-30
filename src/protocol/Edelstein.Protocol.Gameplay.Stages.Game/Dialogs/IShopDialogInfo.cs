846c93de4e4e5a7c3751e219d69a6107023477bf
Kaioru <keith@kaioru.co>
2021-10-30 13:36:39 +0800

    Add ShopDialog interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Dialogs/IShopDialogInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Dialogs
+{
+    public interface IShopDialogInfo : IRepositoryEntry<int>
+    {
+        ICollection<IShopDialogInfoItem> Items { get; }
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Dialogs/IShopDialogInfoItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Game.Dialogs
+{
+    public interface IShopDialogInfoItem
+    {
+        int TemplateID { get; }
+
+        int Price { get; }
+        byte DiscountRate { get; }
+
+        int TokenTemplateID { get; }
+        int TokenPrice { get; }
+
+        int ItemPeriod { get; }
+        int LevelLimited { get; }
+        double UnitPrice { get; }
+        short MaxPerSlot { get; }
+        int Quantity { get; }
+    }
+}
