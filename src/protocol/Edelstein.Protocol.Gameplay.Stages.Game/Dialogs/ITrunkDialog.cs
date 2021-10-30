d2f27d3a016f24b33fe68ca8aa4c2ec1094991bb
Kaioru <keith@kaioru.co>
2021-10-30 14:30:05 +0800

    Add TemplateID property to ShopDialog

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Dialogs/ITrunkDialog.cs
@@ -0,0 +1,10 @@
+﻿using Edelstein.Protocol.Gameplay.Users.Inventories;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Dialogs
+{
+    public interface ITrunkDialog : IDialog
+    {
+        int TemplateID { get; }
+        ItemTrunk Trunk { get; }
+    }
+}
