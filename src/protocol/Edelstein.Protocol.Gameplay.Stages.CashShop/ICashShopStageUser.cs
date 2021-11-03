079827e921af2966fe4bb3def2726665c51ad943
Kaioru <keith@kaioru.co>
2021-11-03 09:44:43 +0800

    Add barebones CashShop and ITC interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.CashShop/ICashShopStageUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.CashShop
+{
+    public interface ICashShopStageUser<TStage, TUser> : IServerStageUser<TStage, TUser>
+        where TStage : ICashShopStage<TStage, TUser>
+        where TUser : ICashShopStageUser<TStage, TUser>
+    {
+    }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.ITC/IITCStage.cs
@@ -0,0 +1,8 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.ITC
+{
+    public interface IITCStage<TStage, TUser> : IServerStage<TStage, TUser>
+        where TStage : IITCStage<TStage, TUser>
+        where TUser : IITCStageUser<TStage, TUser>
+    {
+    }
+}
