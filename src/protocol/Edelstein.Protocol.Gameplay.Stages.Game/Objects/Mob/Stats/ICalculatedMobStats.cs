b8dfacc92db250b77f73ff8a5db27916862950c9
Kaioru <keith@kaioru.co>
2021-11-04 11:08:56 +0800

    Add CalculatedMobStats

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/Mob/Stats/ICalculatedMobStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.Mob.Stats
+{
+    public interface ICalculatedMobStats
+    {
+        int MaxHP { get; }
+        int MaxMP { get; }
+
+        int PAD { get; }
+        int PDR { get; }
+        int MAD { get; }
+        int MDR { get; }
+        int ACC { get; }
+        int EVA { get; }
+
+        Task Calculate();
+    }
+}
