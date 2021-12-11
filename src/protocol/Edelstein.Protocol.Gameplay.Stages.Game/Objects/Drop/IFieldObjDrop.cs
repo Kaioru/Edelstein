7eca28c91b62189f4641be87b196ad2128574547
Kaioru <keith@kaioru.co>
2021-12-11 10:35:59 +0800

    Add Field Drop interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/Drop/IFieldObjDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.Drop
+{
+    public interface IFieldObjDrop : IFieldObj
+    {
+        DropOwnerType OwnerType { get; }
+
+        int OwnerID { get; }
+        int SourceID { get; }
+
+        bool IsMoney { get; }
+        int Info { get; }
+
+        Task PickUp(IFieldObjUser user);
+    }
+}
