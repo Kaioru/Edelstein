03c6be258a96c01d678dc0ffcff2fcda2fc0799c
Kaioru <keith@kaioru.co>
2022-07-14 21:12:39 +0800

    Add stage and loginstage interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay/IStage.cs
@@ -0,0 +1,11 @@
+﻿namespace Edelstein.Protocol.Gameplay;
+
+public interface IStage<TStage, TStageUser>
+    where TStage : IStage<TStage, TStageUser>
+    where TStageUser : IStageUser<TStage, TStageUser>
+{
+    IEnumerable<TStageUser> GetUsers();
+    
+    Task Enter(TStageUser user);
+    Task Leave(TStageUser user);
+}
