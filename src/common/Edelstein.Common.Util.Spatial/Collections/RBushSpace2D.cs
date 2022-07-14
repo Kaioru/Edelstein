173e2b8e81fcbafc9106257037af625e6513dd12
Kaioru <keith@kaioru.co>
2022-07-14 19:51:50 +0800

    Add RBush implementation of Space2D

---
+++ src/common/Edelstein.Common.Util.Spatial/Collections/RBushSpace2D.cs
@@ -0,0 +1,23 @@
+﻿using Edelstein.Protocol.Util.Spatial;
+using Edelstein.Protocol.Util.Spatial.Collections;
+using RBush;
+
+namespace Edelstein.Common.Util.Spatial.Collections;
+
+public class RBushSpace2D<TObject> : ISpace2D<TObject> where TObject : IObject2D
+{
+    private readonly RBush<RBushSpaceObject2D<TObject>> _tree;
+
+    public RBushSpace2D()
+        => _tree = new RBush<RBushSpaceObject2D<TObject>>();
+
+    public IEnumerable<TObject> Find(IObject2D obj)
+        => _tree
+            .Search(new(obj.MinX, obj.MinY, obj.MaxX, obj.MaxY))
+            .Select(o => o.Object);
+
+    public IEnumerable<TObject> FindClosest(IPoint2D point, int n)
+        => _tree
+            .Knn(n, point.X, point.Y)
+            .Select(o => o.Object);
+}
--- src/protocol/Edelstein.Protocol.Util.Spatial/Collections/ISpace2D.cs
+++ src/protocol/Edelstein.Protocol.Util.Spatial/Collections/ISpace2D.cs
@@ -1,7 +1,7 @@
 ﻿namespace Edelstein.Protocol.Util.Spatial.Collections;
 
-public interface ISpace2D<out TSpaceObject> where TSpaceObject : ISpaceObject2D
+public interface ISpace2D<out TObject> where TObject : IObject2D
 {
-    IEnumerable<TSpaceObject> Find(IObject2D obj);
-    IEnumerable<TSpaceObject> FindClosest(IPoint2D point, int n);
+    IEnumerable<TObject> Find(IObject2D obj);
+    IEnumerable<TObject> FindClosest(IPoint2D point, int n);
 }
