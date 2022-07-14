3b22b11d818ffc678a516d26b01189cb5cf17288
Kaioru <keith@kaioru.co>
2022-07-14 21:12:39 +0800

    Add stage and loginstage interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Login/ILoginStageUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Login;
+
+public interface ILoginStageUser<TStage, TStageUser> : IStageUser<TStage, TStageUser>
+    where TStage : ILoginStage<TStage, TStageUser>
+    where TStageUser : ILoginStageUser<TStage, TStageUser>
+{
+    LoginState State { get; set; }
+
+    byte? SelectedWorldID { get; set; }
+    byte? SelectedChannelID { get; set; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay/IStageUser.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Network;
+
+namespace Edelstein.Protocol.Gameplay;
+
+public interface IStageUser<TStage, TStageUser> : ISocketAdapter
+    where TStage : IStage<TStage, TStageUser>
+    where TStageUser : IStageUser<TStage, TStageUser>
+{
+}
