ba70cae141d36260dc289a22bad98670fb9a0a43
Kaioru <keith@kaioru.co>
2022-07-14 19:22:20 +0800

    Change ISpace2D methods to allow retrieval of multiple intersecting results

--- src/protocol/Edelstein.Protocol.Util.Spatial/Collections/ISpace2D.cs
+++ src/protocol/Edelstein.Protocol.Util.Spatial/Collections/ISpace2D.cs
@@ -2,7 +2,6 @@
 
 public interface ISpace2D<out TSpaceObject> where TSpaceObject : ISpaceObject2D
 {
-    TSpaceObject? FindObject(IObject2D obj);
-    TSpaceObject? FindObjectBelow(IObject2D obj);
-    TSpaceObject? FindObjectClosest(IObject2D obj);
+    IEnumerable<TSpaceObject> FindObject(IObject2D obj);
+    IEnumerable<TSpaceObject> FindObjectClosest(IObject2D obj, int n);
 }
