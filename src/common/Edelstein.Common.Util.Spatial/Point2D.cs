396b5af9e68c3bd88c3d69f9799f2a2e3a4eec89
Kaioru <keith@kaioru.co>
2022-07-14 20:09:18 +0800

    Add spatial structs common implementation

---
+++ src/common/Edelstein.Common.Util.Spatial/Point2D.cs
@@ -0,0 +1,23 @@
+﻿using Edelstein.Protocol.Util.Spatial;
+
+namespace Edelstein.Common.Util.Spatial;
+
+public struct Point2D : IPoint2D
+{
+    public int MinX => X;
+    public int MinY => Y;
+    public int MaxX => X;
+    public int MaxY => Y;
+
+    public int X { get; }
+    public int Y { get; }
+
+    public Point2D(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double Distance(IPoint2D point)
+        => Math.Sqrt(Math.Pow(X - point.X, 2) + Math.Pow(Y - point.Y, 2));
+}
