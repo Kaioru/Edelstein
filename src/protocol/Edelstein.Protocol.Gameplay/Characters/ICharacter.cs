ebaa5c800bbe09cedfd39ed4e59f6fb3cc5d29e7
Kaioru <keith@kaioru.co>
2022-07-15 14:06:29 +0800

    Add Account, Character, and Inventory interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay/Characters/ICharacter.cs
@@ -0,0 +1,49 @@
+﻿using Edelstein.Protocol.Gameplay.Inventories;
+using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Characters;
+
+public interface ICharacter : IIdentifiable<int>
+{
+    string Name { get; set; }
+    byte Gender { get; set; }
+    byte Skin { get; set; }
+    int Face { get; set; }
+    int Hair { get; set; }
+
+    long[] Pets { get; set; }
+
+    byte Level { get; set; }
+    short Job { get; set; }
+    short STR { get; set; }
+    short DEX { get; set; }
+    short INT { get; set; }
+    short LUK { get; set; }
+
+    int HP { get; set; }
+    int MaxHP { get; set; }
+    int MP { get; set; }
+    int MaxMP { get; set; }
+
+    short AP { get; set; }
+    short SP { get; set; }
+
+    IDictionary<byte, byte> ExtendSP { get; }
+
+    int EXP { get; set; }
+    short POP { get; set; }
+
+    int Money { get; set; }
+    int TempEXP { get; set; }
+
+    int FieldID { get; set; }
+    byte FieldPortal { get; set; }
+
+    int PlayTime { get; set; }
+
+    short SubJob { get; set; }
+
+    IDictionary<ItemInventoryType, IItemInventory> Inventories { get; }
+
+    public int[] WishList { get; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay/Inventories/Items/IItemSlot.cs
@@ -0,0 +1,7 @@
+﻿using Edelstein.Protocol.Util.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Inventories.Items;
+
+public interface IItemSlot : IIdentifiable<int>
+{
+}
