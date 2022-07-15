f004153b2785536a75b5ffc4dd2e9dfb0ac4cbb3
Kaioru <keith@kaioru.co>
2022-07-15 15:22:15 +0800

    Add NettySocket implementation

---
+++ src/common/Edelstein.Common.Network.DotNetty/NettySocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using DotNetty.Transport.Channels;
+using Edelstein.Protocol.Network;
+using Edelstein.Protocol.Network.Packets;
+
+namespace Edelstein.Common.Network.DotNetty;
+
+public class NettySocket : ISocket
+{
+    private readonly IChannel _channel;
+
+    public string ID => _channel.Id.AsLongText();
+
+    public EndPoint AddressLocal => _channel.LocalAddress;
+    public EndPoint AddressRemote => _channel.RemoteAddress;
+
+    public uint SeqSend { get; set; }
+    public uint SeqRecv { get; set; }
+
+    public bool IsDataEncrypted { get; }
+
+    public NettySocket(IChannel channel, uint seqSend, uint seqRecv, bool isDataEncrypted = true)
+    {
+        _channel = channel ?? throw new ArgumentNullException(nameof(channel));
+        SeqSend = seqSend;
+        SeqRecv = seqRecv;
+        IsDataEncrypted = isDataEncrypted;
+    }
+
+    public Task Dispatch(IPacket packet) => _channel.WriteAndFlushAsync(packet);
+    public Task Close() => _channel.DisconnectAsync();
+}
