4ca9f92a4f951af44bcc8d169a07533f6d3bec4e
Kaioru <keith@kaioru.co>
2022-07-15 17:11:25 +0800

    Fix unused import

--- src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStage.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/LoginStage.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Immutable;
-using Edelstein.Protocol.Gameplay.Stages.Login;
+﻿using Edelstein.Protocol.Gameplay.Stages.Login;
 
 namespace Edelstein.Common.Gameplay.Stages.Login;
 
