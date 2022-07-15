501ac0df1bdcb9cb58e7c3c7b939d28aedbd4579
Kaioru <keith@kaioru.co>
2022-07-15 14:08:55 +0800

    Add ItemSlot interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay/Inventories/Items/IItemSlotBundle.cs
@@ -0,0 +1,9 @@
+﻿namespace Edelstein.Protocol.Gameplay.Inventories.Items;
+
+public interface IItemSlotBundle : IItemSlot
+{
+    short Number { get; set; }
+    short Attribute { get; set; }
+
+    string Title { get; set; }
+}
