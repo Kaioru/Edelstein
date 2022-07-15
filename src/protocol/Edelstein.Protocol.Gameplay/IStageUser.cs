bfe1e1788fcee1bdfa338dab002f1685b08e597b
Kaioru <keith@kaioru.co>
2022-07-15 14:11:56 +0800

    Add Account, AccountWorld, and Character to IStageUser

--- src/protocol/Edelstein.Protocol.Gameplay/IStageUser.cs
+++ src/protocol/Edelstein.Protocol.Gameplay/IStageUser.cs
@@ -1,4 +1,6 @@
-﻿using Edelstein.Protocol.Network;
+﻿using Edelstein.Protocol.Gameplay.Accounts;
+using Edelstein.Protocol.Gameplay.Characters;
+using Edelstein.Protocol.Network;
 using Edelstein.Protocol.Util.Repositories;
 
 namespace Edelstein.Protocol.Gameplay;
@@ -7,4 +9,7 @@ public interface IStageUser<TStage, TStageUser> : IIdentifiable<int>, IAdapter
     where TStage : IStage<TStage, TStageUser>
     where TStageUser : IStageUser<TStage, TStageUser>
 {
+    IAccount? Account { get; set; }
+    IAccountWorld? AccountWorld { get; set; }
+    ICharacter? Character { get; set; }
 }
