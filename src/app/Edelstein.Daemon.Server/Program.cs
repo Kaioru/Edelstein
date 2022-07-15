b7a356535f209c94226d181f142889e736006761
Kaioru <keith@kaioru.co>
2022-07-15 13:21:57 +0800

    Add basic implementation for Edelstein.Daemon.Server

--- src/app/Edelstein.Daemon.Server/Program.cs
+++ src/app/Edelstein.Daemon.Server/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Edelstein.Daemon.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.AddJsonFile("appsettings.json", true);
+        builder.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true);
+        builder.AddCommandLine(args);
+    })
+    .UseSerilog((ctx, logger) => logger.ReadFrom.Configuration(ctx.Configuration))
+    .ConfigureServices((ctx, services) => { services.AddHostedService<ProgramHost>(); })
+    .RunConsoleAsync();
---
+++ src/app/Edelstein.Daemon.Server/ProgramHost.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Edelstein.Daemon.Server;
+
+public class ProgramHost : IHostedService
+{
+    private readonly ILogger<ProgramHost> _logger;
+
+    public ProgramHost(ILogger<ProgramHost> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Host has started");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Host has stopped");
+        return Task.CompletedTask;
+    }
+}
