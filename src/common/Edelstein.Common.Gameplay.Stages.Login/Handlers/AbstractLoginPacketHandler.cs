0631f124de2bc1ed27e756c98c559e187c1afb70
Kaioru <keith@kaioru.co>
2022-07-16 01:33:00 +0800

    Add abstraction for ILoginPacketHandler

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/AbstractLoginPacketHandler.cs
@@ -0,0 +1,13 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Gameplay.Stages.Login.Handlers;
+using Edelstein.Protocol.Network.Packets;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Handlers;
+
+public abstract class AbstractLoginPacketHandler : ILoginPacketHandler
+{
+    public abstract short Operation { get; }
+
+    public virtual bool Check(ILoginStageUser user) => true;
+    public abstract Task Handle(ILoginStageUser user, IPacketReader reader);
+}
--- src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/CheckPasswordHandler.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/CheckPasswordHandler.cs
@@ -1,24 +1,23 @@
 ﻿using Edelstein.Common.Gameplay.Packets;
 using Edelstein.Common.Gameplay.Stages.Login.Messages;
 using Edelstein.Protocol.Gameplay.Stages.Login;
-using Edelstein.Protocol.Gameplay.Stages.Login.Handlers;
 using Edelstein.Protocol.Gameplay.Stages.Login.Messages;
 using Edelstein.Protocol.Network.Packets;
 using Edelstein.Protocol.Util.Pipelines;
 
 namespace Edelstein.Common.Gameplay.Stages.Login.Handlers;
 
-public class CheckPasswordHandler : ILoginPacketHandler
+public class CheckPasswordHandler : AbstractLoginPacketHandler
 {
     private readonly IPipeline<ICheckPassword> _pipeline;
 
     public CheckPasswordHandler(IPipeline<ICheckPassword> pipeline) => _pipeline = pipeline;
 
-    public short Operation => (short)PacketRecvOperations.CheckPassword;
+    public override short Operation => (short)PacketRecvOperations.CheckPassword;
 
-    public bool Check(ILoginStageUser user) => user.State == LoginState.CheckPassword;
+    public override bool Check(ILoginStageUser user) => user.State == LoginState.CheckPassword;
 
-    public Task Handle(ILoginStageUser user, IPacketReader reader)
+    public override Task Handle(ILoginStageUser user, IPacketReader reader)
     {
         var message = new CheckPassword(
             user,
