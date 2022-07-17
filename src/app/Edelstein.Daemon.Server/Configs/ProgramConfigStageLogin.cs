293b8e707c49f7a555f4fe6f1d7d987fa17f5767
Kaioru <keith@kaioru.co>
2022-07-17 13:15:30 +0800

    Add ILoginContextServices

--- src/app/Edelstein.Daemon.Server/Configs/ProgramConfigStageLogin.cs
+++ src/app/Edelstein.Daemon.Server/Configs/ProgramConfigStageLogin.cs
@@ -1,5 +1,7 @@
 ﻿using Edelstein.Protocol.Gameplay.Stages.Login.Contexts;
 
+#pragma warning disable CS8618
+
 namespace Edelstein.Daemon.Server.Configs;
 
 public record ProgramConfigStageLogin : AbstractProgramConfigStage, ILoginContextOptions;
--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Login/Contexts/ILoginContext.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Login/Contexts/ILoginContext.cs
@@ -4,4 +4,5 @@ public interface ILoginContext
 {
     ILoginContextOptions Options { get; }
     ILoginContextPipelines Pipelines { get; }
+    ILoginContextServices Services { get; }
 }
