c618f9f68954763ccb2d03a42af457d23d74705d
Kaioru <keith@kaioru.co>
2022-07-17 23:48:48 +0800

    Add SocketOnDisconnect action

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Actions/SocketOnDisconnectAction.cs
@@ -0,0 +1,8 @@
+﻿using Edelstein.Common.Gameplay.Stages.Actions;
+using Edelstein.Protocol.Gameplay.Stages.Login;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Actions;
+
+public class SocketOnDisconnectAction : AbstractSocketOnDisconnectAction<ILoginStageUser>
+{
+}
