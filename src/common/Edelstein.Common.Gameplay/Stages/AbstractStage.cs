2a91c984ca2f565b01cb4dcd71d597e0e0673d0e
Kaioru <keith@kaioru.co>
2022-07-17 23:48:48 +0800

    Add SocketOnDisconnect action

--- src/common/Edelstein.Common.Gameplay/Stages/AbstractStage.cs
+++ src/common/Edelstein.Common.Gameplay/Stages/AbstractStage.cs
@@ -4,13 +4,13 @@ namespace Edelstein.Common.Gameplay.Stages;
 
 public abstract class AbstractStage<TStageUser> : IStage<TStageUser> where TStageUser : IStageUser<TStageUser>
 {
-    public virtual Task Enter(TStageUser user)
+    public Task Enter(TStageUser user)
     {
         user.Stage = this;
         return Task.CompletedTask;
     }
 
-    public virtual Task Leave(TStageUser user)
+    public Task Leave(TStageUser user)
     {
         user.Stage = null;
         return Task.CompletedTask;
---
+++ src/common/Edelstein.Common.Gameplay/Stages/Actions/AbstractSocketOnDisconnectAction.cs
@@ -0,0 +1,12 @@
+﻿using Edelstein.Protocol.Gameplay.Stages;
+using Edelstein.Protocol.Gameplay.Stages.Messages;
+using Edelstein.Protocol.Util.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Stages.Actions;
+
+public abstract class AbstractSocketOnDisconnectAction<TStageUser> : IPipelineAction<ISocketOnDisconnect<TStageUser>>
+    where TStageUser : IStageUser<TStageUser>
+{
+    public Task Handle(IPipelineContext ctx, ISocketOnDisconnect<TStageUser> message) =>
+        Task.FromResult(message.User.Stage?.Leave(message.User));
+}
