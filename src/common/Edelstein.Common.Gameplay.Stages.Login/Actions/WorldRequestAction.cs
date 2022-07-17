7d78f803aed3a45c00768b2a84b50cffe4dc5626
Kaioru <keith@kaioru.co>
2022-07-17 23:11:05 +0800

    Add IWorldRequest interfaces

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Actions/WorldRequestAction.cs
@@ -0,0 +1,18 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Login.Messages;
+using Edelstein.Protocol.Util.Pipelines;
+using Microsoft.Extensions.Logging;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Actions;
+
+public class WorldRequestAction : IPipelineAction<IWorldRequest>
+{
+    private readonly ILogger _logger;
+
+    public WorldRequestAction(ILogger<WorldRequestAction> logger) => _logger = logger;
+
+    public Task Handle(IPipelineContext ctx, IWorldRequest message)
+    {
+        _logger.LogInformation("Called world request");
+        return Task.CompletedTask;
+    }
+}
