06f2f9e21f3baaeb4dd4875084658ae0602009e9
Kaioru <keith@kaioru.co>
2022-07-17 22:38:08 +0800

    Add unique index to Identity and Account Username

---
+++ src/common/Edelstein.Common.Gameplay.Database/Migrations/20220717143458_AddUniqueIndexToAccountUsername.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edelstein.Common.Gameplay.Database.Migrations
+{
+    public partial class AddUniqueIndexToAccountUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Username",
+                table: "Accounts",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Username",
+                table: "Accounts");
+        }
+    }
+}
---
+++ src/common/Edelstein.Common.Services.Auth/Migrations/20220717143612_AddUniqueIndexToIdentityUsername.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edelstein.Common.Services.Auth.Migrations
+{
+    public partial class AddUniqueIndexToIdentityUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Identities_Username",
+                table: "Identities",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Identities_Username",
+                table: "Identities");
+        }
+    }
+}
