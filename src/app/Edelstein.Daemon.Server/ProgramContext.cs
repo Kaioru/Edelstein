e212e4afb3e1ea42ad9ac92c577d9036bb77bc82
Kaioru <keith@kaioru.co>
2022-07-18 13:43:52 +0800

    Add logic for WorldRequestAction, move template loading to be stage specific

--- src/app/Edelstein.Daemon.Server/ProgramContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Edelstein.Common.Util.Templates;
-using Edelstein.Protocol.Gameplay.Stages.Login.Contexts;
-
-namespace Edelstein.Daemon.Server;
-
-public record ProgramContext(
-    IEnumerable<ITemplateLoader> TemplateLoaders,
-    ILoginContextTemplates LoginTemplates
-);
