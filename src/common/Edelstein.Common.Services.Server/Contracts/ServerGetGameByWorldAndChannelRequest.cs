ab9692c9919c3177e5e7d9219274445efc1c0740
Kaioru <keith@kaioru.co>
2022-07-30 22:33:27 +0800

    Add implementations of server service contracts

---
+++ src/common/Edelstein.Common.Services.Server/Contracts/ServerGetGameByWorldAndChannelRequest.cs
@@ -0,0 +1,8 @@
+﻿using Edelstein.Protocol.Services.Server.Contracts;
+
+namespace Edelstein.Common.Services.Server.Contracts;
+
+public record ServerGetGameByWorldAndChannelRequest(
+    int WorldID,
+    int ChannelID
+) : IServerGetGameByWorldAndChannelRequest;
