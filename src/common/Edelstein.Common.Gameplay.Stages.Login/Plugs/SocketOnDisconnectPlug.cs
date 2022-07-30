854b1fff73ebeed4c89233370d3ef40c87f9cbcb
Kaioru <keith@kaioru.co>
2022-07-31 00:22:17 +0800

    Add session start in CheckPassword plug and session end on disconnect

--- src/common/Edelstein.Common.Gameplay.Stages.Login/Plugs/SocketOnDisconnectPlug.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Plugs/SocketOnDisconnectPlug.cs
@@ -1,8 +1,12 @@
 ﻿using Edelstein.Common.Gameplay.Stages.Actions;
 using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Services.Session;
 
 namespace Edelstein.Common.Gameplay.Stages.Login.Plugs;
 
 public class SocketOnDisconnectPlug : AbstractSocketOnDisconnectPlug<ILoginStageUser>
 {
+    public SocketOnDisconnectPlug(ISessionService session) : base(session)
+    {
+    }
 }
