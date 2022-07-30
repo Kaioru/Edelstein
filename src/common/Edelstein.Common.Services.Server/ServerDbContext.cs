59c9336988fd4e61af3eeb38d4cffe63a39e76b3
Kaioru <keith@kaioru.co>
2022-07-30 11:52:35 +0800

    Add SessionModel and MigrationModel

--- src/common/Edelstein.Common.Services.Server/ServerDbContext.cs
+++ src/common/Edelstein.Common.Services.Server/ServerDbContext.cs
@@ -15,10 +15,28 @@ public class ServerDbContext : DbContext
     public DbSet<ServerLoginModel> LoginServers { get; set; }
     public DbSet<ServerGameModel> GameServers { get; set; }
 
+    public DbSet<SessionModel> Sessions { get; set; }
+    public DbSet<MigrationModel> Migrations { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Entity<ServerModel>().HasDiscriminator().HasValue("Server");
         builder.Entity<ServerLoginModel>().HasDiscriminator().HasValue("Login");
         builder.Entity<ServerGameModel>().HasDiscriminator().HasValue("Game");
+
+        builder.Entity<SessionModel>().HasKey(m => m.ActiveAccount);
+        builder.Entity<SessionModel>()
+            .HasOne(m => m.Server)
+            .WithMany(p => p.Sessions)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<MigrationModel>()
+            .HasOne(m => m.FromServer)
+            .WithMany(p => p.MigrationOut)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<MigrationModel>()
+            .HasOne(m => m.ToServer)
+            .WithMany(p => p.MigrationIn)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
