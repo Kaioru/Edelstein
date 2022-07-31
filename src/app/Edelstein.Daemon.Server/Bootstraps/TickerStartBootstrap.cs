d7ddc66f1a8eed4ade42cfeccb37c7f5e40165e5
Kaioru <keith@kaioru.co>
2022-08-01 00:03:21 +0800

    Rework Edelstein.Daemon.Server startup process

---
+++ src/app/Edelstein.Daemon.Server/Bootstraps/TickerStartBootstrap.cs
@@ -0,0 +1,13 @@
+﻿using Edelstein.Protocol.Util.Tickers;
+
+namespace Edelstein.Daemon.Server.Bootstraps;
+
+public class TickerStartBootstrap : IBootstrap
+{
+    private readonly ITickerManager _ticker;
+
+    public TickerStartBootstrap(ITickerManager ticker) => _ticker = ticker;
+
+    public Task Start() => _ticker.Start();
+    public Task Stop() => _ticker.Stop();
+}
--- src/app/Edelstein.Daemon.Server/Program.cs
+++ src/app/Edelstein.Daemon.Server/Program.cs
@@ -1,14 +1,37 @@
-﻿using Edelstein.Common.Data.NX;
+﻿using System.Collections.Immutable;
+using Edelstein.Common.Data.NX;
+using Edelstein.Common.Gameplay.Accounts;
 using Edelstein.Common.Gameplay.Database;
+using Edelstein.Common.Gameplay.Database.Repositories;
+using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Common.Gameplay.Stages.Login;
+using Edelstein.Common.Gameplay.Stages.Login.Contexts;
+using Edelstein.Common.Network.DotNetty.Transports;
 using Edelstein.Common.Services.Auth;
 using Edelstein.Common.Services.Server;
+using Edelstein.Common.Util.Pipelines;
+using Edelstein.Common.Util.Templates;
+using Edelstein.Common.Util.Tickers;
 using Edelstein.Daemon.Server;
+using Edelstein.Daemon.Server.Bootstraps;
 using Edelstein.Daemon.Server.Configs;
 using Edelstein.Protocol.Data;
+using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Gameplay.Stages.Login.Contexts;
+using Edelstein.Protocol.Network;
+using Edelstein.Protocol.Services.Auth;
+using Edelstein.Protocol.Services.Migration;
+using Edelstein.Protocol.Services.Server;
+using Edelstein.Protocol.Services.Session;
+using Edelstein.Protocol.Util.Pipelines;
+using Edelstein.Protocol.Util.Templates;
+using Edelstein.Protocol.Util.Tickers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 
 await Host.CreateDefaultBuilder(args)
@@ -21,8 +44,6 @@ await Host.CreateDefaultBuilder(args)
     .UseSerilog((ctx, logger) => logger.ReadFrom.Configuration(ctx.Configuration))
     .ConfigureServices((ctx, services) =>
     {
-        services.AddSingleton(services);
-
         services.AddPooledDbContextFactory<AuthDbContext>(o =>
             o.UseNpgsql(ctx.Configuration.GetConnectionString(AuthDbContext.ConnectionStringKey)));
         services.AddPooledDbContextFactory<ServerDbContext>(o =>
@@ -30,9 +51,90 @@ await Host.CreateDefaultBuilder(args)
         services.AddPooledDbContextFactory<GameplayDbContext>(o =>
             o.UseNpgsql(ctx.Configuration.GetConnectionString(GameplayDbContext.ConnectionStringKey)));
 
-        services.AddSingleton<IDataManager>(new NXDataManager(ctx.Configuration.GetSection("Data")["Directory"]));
+        services.AddSingleton<IAccountRepository, AccountRepository>();
 
-        services.Configure<ProgramConfig>(ctx.Configuration.GetSection("Host"));
-        services.AddHostedService<ProgramHost>();
+        services.AddSingleton<IAuthService, AuthService>();
+        services.AddSingleton<IServerService, ServerService>();
+        services.AddSingleton<ISessionService, SessionService>();
+        services.AddSingleton<IMigrationService, MigrationService>();
     })
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddSingleton<IDataManager>(new NXDataManager(ctx.Configuration.GetSection("Data")["Directory"]));
+        services.AddSingleton(typeof(ITemplateManager<>), typeof(TemplateManager<>));
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddSingleton<ITickerManager, TickerManager>();
+        services.AddSingleton<IBootstrap, TickerStartBootstrap>();
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        var config = ctx.Configuration.GetSection("Host").Get<ProgramConfig>();
+        var stages = new List<AbstractProgramConfigStage>();
+
+        services.AddSingleton(config);
+        stages.AddRange(config.LoginStages);
+
+        foreach (var stage in stages)
+            services.AddSingleton<IBootstrap>(p =>
+            {
+                var scope = new ServiceCollection { services };
+
+                scope.Scan(s => s
+                    .FromApplicationDependencies()
+                    .AddClasses(c => c.AssignableTo(typeof(IPacketHandler<>)))
+                    .AsImplementedInterfaces()
+                    .WithSingletonLifetime()
+                    .AddClasses(c => c.AssignableTo(typeof(IPipelinePlug<>)))
+                    .AsImplementedInterfaces()
+                    .WithSingletonLifetime()
+                );
+                scope.AddSingleton(typeof(IPacketHandlerManager<>), typeof(PacketHandlerManager<>));
+                scope.AddSingleton(typeof(IPipeline<>), typeof(Pipeline<>));
+
+                switch (stage)
+                {
+                    case ILoginContextOptions options:
+                        scope.Scan(s => s
+                            .FromAssemblyOf<LoginStage>()
+                            .AddClasses(c => c.AssignableTo<ITemplateLoader>())
+                            .AsImplementedInterfaces()
+                            .WithSingletonLifetime()
+                        );
+
+                        scope.AddSingleton(options);
+                        scope.AddSingleton<ILoginContext, LoginContext>();
+                        scope.AddSingleton<ILoginContextPipelines, LoginContextPipelines>();
+                        scope.AddSingleton<ILoginContextServices, LoginContextServices>();
+                        scope.AddSingleton<ILoginContextTemplates, LoginContextTemplates>();
+                        scope.AddSingleton<IAdapterInitializer, LoginStageUserInitializer>();
+                        scope.AddSingleton<ILoginStage, LoginStage>();
+                        break;
+                }
+
+                var provider = scope.BuildServiceProvider();
+                var adapter = provider.GetRequiredService<IAdapterInitializer>();
+                var acceptor = new NettyTransportAcceptor(adapter, stage.Version, stage.Patch, stage.Locale);
+                var loaders = provider.GetServices<ITemplateLoader>();
+
+                return new ServerStartBootstrap(
+                    p.GetRequiredService<ILogger<ServerStartBootstrap>>(),
+                    acceptor,
+                    stage,
+                    loaders.ToImmutableList()
+                );
+            });
+
+        foreach (var login in config.LoginStages)
+            services.AddSingleton<IBootstrap>(p =>
+                new ServerUpdateBootstrap<ProgramConfigStageLogin>(
+                    p.GetRequiredService<ILogger<ServerUpdateBootstrap<ProgramConfigStageLogin>>>(),
+                    login,
+                    p.GetRequiredService<IServerService>(),
+                    p.GetRequiredService<ITickerManager>()
+                )
+            );
+    })
+    .ConfigureServices((ctx, services) => { services.AddHostedService<ProgramHost>(); })
     .RunConsoleAsync();
