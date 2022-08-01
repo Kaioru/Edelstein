62c3011d233f90cc6dbbeeb30f0b9bb1ad62a20b
Kaioru <keith@kaioru.co>
2022-08-01 22:16:56 +0800

    Add OnDisconnect and OnPacket plug to GameStage

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/Plugs/SocketOnDisconnectPlug.cs
@@ -0,0 +1,19 @@
+﻿using Edelstein.Common.Gameplay.Accounts;
+using Edelstein.Common.Gameplay.Characters;
+using Edelstein.Common.Gameplay.Stages.Actions;
+using Edelstein.Protocol.Gameplay.Stages.Game;
+using Edelstein.Protocol.Services.Session;
+
+namespace Edelstein.Common.Gameplay.Stages.Game.Plugs;
+
+public class SocketOnDisconnectPlug : AbstractSocketOnDisconnectPlug<IGameStageUser>
+{
+    public SocketOnDisconnectPlug(
+        ISessionService session,
+        IAccountRepository accountRepository,
+        IAccountWorldRepository accountWorldRepository,
+        ICharacterRepository characterRepository
+    ) : base(session, accountRepository, accountWorldRepository, characterRepository)
+    {
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/Plugs/SocketOnPacketPlug.cs
@@ -0,0 +1,12 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Common.Gameplay.Stages.Actions;
+using Edelstein.Protocol.Gameplay.Stages.Game;
+
+namespace Edelstein.Common.Gameplay.Stages.Game.Plugs;
+
+public class SocketOnPacketPlug : AbstractSocketOnPacketPlug<IGameStageUser>
+{
+    public SocketOnPacketPlug(IPacketHandlerManager<IGameStageUser> handler) : base(handler)
+    {
+    }
+}
