a39e84a510bdf99d223a6bd0c9bf9cd8fc0956a2
Kaioru <keith@kaioru.co>
2022-08-01 11:57:28 +0800

    Change TemplateProviderLazy to use WeakReferences

---
+++ src/common/Edelstein.Common.Util.Templates/TemplateProviderLazyHolder.cs
@@ -0,0 +1,7 @@
+﻿using Edelstein.Protocol.Util.Templates;
+
+namespace Edelstein.Common.Util.Templates;
+
+public record TemplateProviderLazyHolder<TTemplate>(
+    TTemplate Template
+) where TTemplate : ITemplate;
