fae9f8155d86dd6838ba5969106c157d767941a1
Kaioru <keith@kaioru.co>
2022-08-01 11:57:28 +0800

    Change TemplateProviderLazy to use WeakReferences

--- src/common/Edelstein.Common.Util.Templates/TemplateProviderLazy.cs
+++ src/common/Edelstein.Common.Util.Templates/TemplateProviderLazy.cs
@@ -5,15 +5,20 @@ namespace Edelstein.Common.Util.Templates;
 public class TemplateProviderLazy<TTemplate> : ITemplateProvider<TTemplate> where TTemplate : ITemplate
 {
     private readonly Func<TTemplate> _func;
+    private readonly WeakReference _reference;
 
     public TemplateProviderLazy(int id, Func<TTemplate> func)
     {
         ID = id;
         _func = func;
+        _reference = new WeakReference(null);
     }
 
-    private TTemplate? Template { get; set; }
     public int ID { get; }
 
-    public Task<TTemplate> Provide() => Task.FromResult(Template ??= _func.Invoke());
+    public Task<TTemplate> Provide()
+        => Task.FromResult(
+            ((TemplateProviderLazyHolder<TTemplate>)(_reference.Target ??=
+                new TemplateProviderLazyHolder<TTemplate>(_func.Invoke())))
+            .Template);
 }
