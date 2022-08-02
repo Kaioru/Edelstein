fa2fbebc8c3c321b7aa94e90fa34e7b96eee713b
Kaioru <keith@kaioru.co>
2022-08-02 10:53:52 +0800

    Add item templates and item template to slot converters

---
+++ src/protocol/Edelstein.Protocol.Gameplay/Inventories/Templates/IItemTemplate.cs
@@ -0,0 +1,24 @@
+﻿using Edelstein.Protocol.Util.Templates;
+
+namespace Edelstein.Protocol.Gameplay.Inventories.Templates;
+
+public interface IItemTemplate : ITemplate
+{
+    int SellPrice { get; }
+    bool TimeLimited { get; }
+
+    int ReplaceTemplateID { get; }
+    int ReplaceMsg { get; }
+    int ReplacePeriod { get; }
+
+    bool Quest { get; }
+    bool PartyQuest { get; }
+    bool Only { get; }
+    bool TradeBlock { get; }
+    bool NotSale { get; }
+    bool BigSize { get; }
+    bool ExpireOnLogout { get; }
+    bool AccountSharable { get; }
+
+    bool Cash { get; }
+}
