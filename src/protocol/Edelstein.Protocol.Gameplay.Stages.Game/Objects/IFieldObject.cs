cb1bbb342ab30ff23e6e7a99151e49fa3541590f
Kaioru <keith@kaioru.co>
2022-08-02 17:11:49 +0800

    Add draft IField interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/IFieldObject.cs
@@ -0,0 +1,20 @@
+﻿using Edelstein.Protocol.Util.Buffers.Bytes;
+using Edelstein.Protocol.Util.Spatial;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects;
+
+public interface IFieldObject
+{
+    FieldObjectType Type { get; }
+
+    int? ObjectID { get; set; }
+
+    IField? Field { get; }
+    IPoint2D Position { get; }
+
+    Task OnEnterField(int id, IField field);
+    Task OnLeaveField();
+
+    IPacket GetEnterFieldPacket();
+    IPacket GetLeaveFieldPacket();
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Templates/IFieldTemplate.cs
@@ -0,0 +1,21 @@
+﻿using Edelstein.Protocol.Util.Spatial;
+using Edelstein.Protocol.Util.Templates;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Templates;
+
+public interface IFieldTemplate : ITemplate
+{
+    FieldLimitType Limit { get; }
+
+    IRectangle2D Bounds { get; }
+
+    int? FieldReturn { get; }
+    int? ForcedReturn { get; }
+
+    string ScriptFirstUserEnter { get; }
+    string ScriptUserEnter { get; }
+
+    double MobRate { get; }
+    int MobCapacityMin { get; }
+    int MobCapacityMax { get; }
+}
