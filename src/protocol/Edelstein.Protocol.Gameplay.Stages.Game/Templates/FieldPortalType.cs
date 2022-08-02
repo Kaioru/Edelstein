d03110a41cea57a26a19ff5e0fccf2b385e8cc77
Kaioru <keith@kaioru.co>
2022-08-02 18:14:54 +0800

    Add IFieldPortal interfaces and loading

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Templates/FieldPortalType.cs
@@ -0,0 +1,19 @@
+﻿namespace Edelstein.Protocol.Gameplay.Stages.Game.Templates;
+
+public enum FieldPortalType
+{
+    StartPoint = 0x0,
+    Invisible = 0x1,
+    Visible = 0x2,
+    Collision = 0x3,
+    Changable = 0x4,
+    ChangableInvisible = 0x5,
+    TownPortalPoint = 0x6,
+    Script = 0x7,
+    ScriptInvisible = 0x8,
+    CollisionScript = 0x9,
+    Hidden = 0xA,
+    ScriptHidden = 0xB,
+    CollisionVerticalJump = 0xC,
+    OllisionCustomImpact = 0xD
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Templates/IFieldTemplatePortal.cs
@@ -0,0 +1,16 @@
+﻿using Edelstein.Protocol.Util.Spatial;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Templates;
+
+public interface IFieldTemplatePortal : IFieldSpaceObject
+{
+    FieldPortalType Type { get; }
+
+    string Name { get; }
+    string? Script { get; }
+
+    int ToMap { get; }
+    string? ToName { get; }
+
+    IPoint2D Position { get; }
+}
