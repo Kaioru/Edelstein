14459331e9c4da18d0e24220fa8fb8dbc5f19787
Kaioru <keith@kaioru.co>
2022-08-03 15:59:37 +0800

    Add MigrateOutPlug sample for sample login plugin

---
+++ src/plugin/Edelstein.Plugin.Login.Sample/Plugs/SampleSocketOnMigrateOutPlug.cs
@@ -0,0 +1,22 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Gameplay.Stages.Messages;
+using Edelstein.Protocol.Util.Pipelines;
+using Microsoft.Extensions.Logging;
+
+namespace Edelstein.Plugin.Login.Sample.Plugs;
+
+public class SampleSocketOnMigrateOutPlug : IPipelinePlug<ISocketOnMigrateOut<ILoginStageUser>>
+{
+    private readonly ILogger _logger;
+
+    public SampleSocketOnMigrateOutPlug(ILogger logger) => _logger = logger;
+
+    public Task Handle(IPipelineContext ctx, ISocketOnMigrateOut<ILoginStageUser> message)
+    {
+        _logger.LogInformation(
+            "{Name} is migrating to {Server}",
+            message.User.Character!.Name, message.ServerID
+        );
+        return Task.CompletedTask;
+    }
+}
