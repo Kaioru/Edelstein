d90734cffe9a95b0704aad8b39e506e90855296a
Kaioru <keith@kaioru.co>
2022-08-03 22:44:39 +0800

    Add method to add pipeline plugs without priority

--- src/protocol/Edelstein.Protocol.Util.Pipelines/IPipeline.cs
+++ src/protocol/Edelstein.Protocol.Util.Pipelines/IPipeline.cs
@@ -3,6 +3,7 @@
 public interface IPipeline<TMessage>
 {
     void Add(int priority, IPipelinePlug<TMessage> plug);
+    void Add(IPipelinePlug<TMessage> plug);
     void Remove(IPipelinePlug<TMessage> plug);
 
     Task Process(TMessage message);
