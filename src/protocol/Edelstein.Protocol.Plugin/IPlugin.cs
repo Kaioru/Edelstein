1d89dd1abb1fb474d27604e272171d8293696280
Kaioru <keith@kaioru.co>
2022-08-03 15:42:27 +0800

    Change plugins to use own type as logger type

--- src/protocol/Edelstein.Protocol.Plugin/IPlugin.cs
+++ src/protocol/Edelstein.Protocol.Plugin/IPlugin.cs
@@ -5,5 +5,5 @@ namespace Edelstein.Protocol.Plugin;
 public interface IPlugin<in TContext>
 {
     Task OnStart(ILogger logger, TContext ctx);
-    Task OnStop(ILogger logger);
+    Task OnStop();
 }
