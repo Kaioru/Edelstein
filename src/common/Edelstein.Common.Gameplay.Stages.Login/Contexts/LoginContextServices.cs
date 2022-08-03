e80dead43aee5233c3cbd20367c7ec4fbbcbd81e
Kaioru <keith@kaioru.co>
2022-08-03 18:19:46 +0800

    Add more services to ILoginContextServices

--- src/common/Edelstein.Common.Gameplay.Stages.Login/Contexts/LoginContextServices.cs
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Contexts/LoginContextServices.cs
@@ -1,8 +1,14 @@
 ﻿using Edelstein.Protocol.Gameplay.Stages.Login.Contexts;
 using Edelstein.Protocol.Services.Auth;
+using Edelstein.Protocol.Services.Migration;
+using Edelstein.Protocol.Services.Server;
+using Edelstein.Protocol.Services.Session;
 
 namespace Edelstein.Common.Gameplay.Stages.Login.Contexts;
 
 public record LoginContextServices(
-    IAuthService Auth
+    IAuthService Auth,
+    IServerService Server,
+    ISessionService Session,
+    IMigrationService Migration
 ) : ILoginContextServices;
--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Login/Contexts/ILoginContextServices.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Login/Contexts/ILoginContextServices.cs
@@ -1,8 +1,14 @@
 ﻿using Edelstein.Protocol.Services.Auth;
+using Edelstein.Protocol.Services.Migration;
+using Edelstein.Protocol.Services.Server;
+using Edelstein.Protocol.Services.Session;
 
 namespace Edelstein.Protocol.Gameplay.Stages.Login.Contexts;
 
 public interface ILoginContextServices
 {
     IAuthService Auth { get; }
+    IServerService Server { get; }
+    ISessionService Session { get; }
+    IMigrationService Migration { get; }
 }
