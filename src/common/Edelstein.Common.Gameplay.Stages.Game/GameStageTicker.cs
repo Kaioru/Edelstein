bd9d1ce324d040149da7265362085c7ebedd626d
Kaioru <keith@kaioru.co>
2022-08-03 11:24:29 +0800

    Change ServerStartBootstrap to allow starting of multiple tickables

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/GameStageTicker.cs
@@ -0,0 +1,8 @@
+﻿using Edelstein.Protocol.Util.Tickers;
+
+namespace Edelstein.Common.Gameplay.Stages.Game;
+
+public class GameStageTicker : ITickable
+{
+    public Task OnTick(DateTime now) => Task.CompletedTask;
+}
