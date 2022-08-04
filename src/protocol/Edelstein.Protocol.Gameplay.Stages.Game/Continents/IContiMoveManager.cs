8dc9c044d54b480b1203c22c6125c42cd3095e5c
Kaioru <keith@kaioru.co>
2022-08-04 19:15:17 +0800

    Add retrieve all to contimove

--- src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Continents/IContiMoveManager.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Continents/IContiMoveManager.cs
@@ -4,7 +4,8 @@ namespace Edelstein.Protocol.Gameplay.Stages.Game.Continents;
 
 public interface IContiMoveManager :
     IRepositoryMethodInsert<int, IContiMove>,
-    IRepositoryMethodRetrieve<int, IContiMove>
+    IRepositoryMethodRetrieve<int, IContiMove>,
+    IRepositoryMethodRetrieveAll<int, IContiMove>
 {
     Task<IContiMove?> RetrieveByName(string name);
     Task<IContiMove?> RetrieveByField(IField field);
