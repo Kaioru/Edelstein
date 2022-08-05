1a655d2f216fc5ffa438de5425c7a7a5ff13400c
Kaioru <keith@kaioru.co>
2022-08-05 15:19:00 +0800

    Add IFieldMessage interfaces

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/Objects/User/Messages/SystemMessage.cs
@@ -0,0 +1,14 @@
+﻿using Edelstein.Protocol.Util.Buffers.Packets;
+
+namespace Edelstein.Common.Gameplay.Stages.Game.Objects.User.Messages;
+
+public class SystemMessage : AbstractFieldMessage
+{
+    private readonly string _message;
+
+    public SystemMessage(string message) => _message = message;
+
+    protected override FieldMessageType Type => FieldMessageType.SystemMessage;
+
+    protected override void WriteData(IPacketWriter writer) => writer.WriteString(_message);
+}
