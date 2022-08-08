85fb9b66b433a85cf680f9224bfb5969dbcf1854
Kaioru <keith@kaioru.co>
2022-08-08 13:18:40 +0800

    Add IUserChangeSlotPositionRequest contracts and plugs

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/Objects/User/Contracts/UserChangeSlotPositionRequest.cs
@@ -0,0 +1,13 @@
+﻿using Edelstein.Protocol.Gameplay.Inventories;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Contracts;
+
+namespace Edelstein.Common.Gameplay.Stages.Game.Objects.User.Contracts;
+
+public record UserChangeSlotPositionRequest(
+    IFieldUser User,
+    ItemInventoryType Type,
+    short From,
+    short To,
+    short Count
+) : IUserChangeSlotPositionRequest;
---
+++ src/common/Edelstein.Common.Gameplay.Stages.Game/Objects/User/Handlers/UserChangeSlotPositionRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Common.Gameplay.Stages.Game.Objects.User.Contracts;
+using Edelstein.Protocol.Gameplay.Inventories;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.User.Contracts;
+using Edelstein.Protocol.Util.Buffers.Packets;
+using Edelstein.Protocol.Util.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Stages.Game.Objects.User.Handlers;
+
+public class UserChangeSlotPositionRequestHandler : AbstractFieldUserHandler
+{
+    private readonly IPipeline<IUserChangeSlotPositionRequest> _pipeline;
+
+    public UserChangeSlotPositionRequestHandler(IPipeline<IUserChangeSlotPositionRequest> pipeline) =>
+        _pipeline = pipeline;
+
+    public override short Operation => (short)PacketRecvOperations.UserChangeSlotPositionRequest;
+
+    protected override Task Handle(IFieldUser user, IPacketReader reader)
+    {
+        _ = reader.ReadInt();
+
+        var message = new UserChangeSlotPositionRequest(
+            user,
+            (ItemInventoryType)reader.ReadByte(),
+            reader.ReadShort(),
+            reader.ReadShort(),
+            reader.ReadShort()
+        );
+
+        return _pipeline.Process(message);
+    }
+}
