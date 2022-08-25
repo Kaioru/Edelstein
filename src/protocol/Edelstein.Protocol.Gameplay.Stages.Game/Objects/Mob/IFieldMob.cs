5a6f498cd37f4a7e4d993ceb3b2ec6713327238b
Kaioru <keith@kaioru.co>
2022-08-25 20:57:04 +0800

    Add IFieldMob interface

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/Mob/IFieldMob.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Game.Objects.Mob.Movements;
+using Edelstein.Protocol.Gameplay.Stages.Game.Objects.Mob.Templates;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.Mob;
+
+public interface IFieldMob : IFieldLife<IMobMovePath, IMobMoveAction>, IFieldControllable
+{
+    IMobTemplate Template { get; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Stages.Game/Objects/Mob/Movements/IMobMoveAction.cs
@@ -0,0 +1,7 @@
+﻿using Edelstein.Protocol.Gameplay.Stages.Game.Movements;
+
+namespace Edelstein.Protocol.Gameplay.Stages.Game.Objects.Mob.Movements;
+
+public interface IMobMoveAction : IMoveAction
+{
+}
