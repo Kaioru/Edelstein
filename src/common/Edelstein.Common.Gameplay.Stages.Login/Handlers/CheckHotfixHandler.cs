bdca2c15cf9a22b50a31e9b513b3c0ee207c0b41
Kaioru <keith@kaioru.co>
2022-09-06 10:52:22 +0800

    Add CheckHotfixHandler, UseAuthServerHandler, and PrivateServerPacketHandler

---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/CheckHotfixHandler.cs
@@ -0,0 +1,21 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Common.Util.Buffers.Packets;
+using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Util.Buffers.Packets;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Handlers;
+
+public class CheckHotfixHandler : AbstractLoginPacketHandler
+{
+    public override short Operation => (short)PacketRecvOperations.CheckHotfix;
+
+    public override Task Handle(ILoginStageUser user, IPacketReader reader)
+    {
+        var packet = new PacketWriter();
+
+        packet.WriteShort(36);
+        packet.WriteBool(true);
+
+        return user.Dispatch(packet);
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay.Stages.Login/Handlers/PrivateServerPacketHandler.cs
@@ -0,0 +1,21 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Common.Util.Buffers.Packets;
+using Edelstein.Protocol.Gameplay.Stages.Login;
+using Edelstein.Protocol.Util.Buffers.Packets;
+
+namespace Edelstein.Common.Gameplay.Stages.Login.Handlers;
+
+public class PrivateServerPacketHandler : AbstractLoginPacketHandler
+{
+    public override short Operation => (short)PacketRecvOperations.PrivateServerPacket;
+
+    public override Task Handle(ILoginStageUser user, IPacketReader reader)
+    {
+        var packet = new PacketWriter();
+
+        packet.WriteShort(23);
+        packet.WriteInt(23 ^ reader.ReadInt());
+
+        return user.Dispatch(packet);
+    }
+}
