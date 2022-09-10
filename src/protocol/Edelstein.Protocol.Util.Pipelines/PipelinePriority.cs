4fbcda91d1fcd4797022d28306256b100984295d
Kaioru <keith@kaioru.co>
2022-09-10 21:30:44 +0800

    Change Pipelines to allow actions to run after default

--- src/protocol/Edelstein.Protocol.Util.Pipelines/PipelinePriority.cs
+++ src/protocol/Edelstein.Protocol.Util.Pipelines/PipelinePriority.cs
@@ -7,4 +7,8 @@ public static class PipelinePriority
     public const int Normal = 0x30;
     public const int Low = 0x40;
     public const int Lowest = 0x50;
+
+    public const int Default = 0x100;
+
+    public const int After = 0x200;
 }
