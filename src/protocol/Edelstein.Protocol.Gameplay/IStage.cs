483bad2006bfde3b706363c78dcf362132b6be11
Kaioru <keith@kaioru.co>
2022-12-15 13:31:57 +0800

    Add Stage interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay/IStage.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Util.Storages;
+
+namespace Edelstein.Protocol.Gameplay;
+
+public interface IStage : IIdentifiable<string>
+{
+    Task Enter(IStageUser user);
+    Task Leave(IStageUser user);
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay/IStageUser.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Network;
+using Edelstein.Protocol.Util.Storages;
+
+namespace Edelstein.Protocol.Gameplay;
+
+public interface IStageUser : IIdentifiable<int>, INetworkAdapter
+{
+    IStage Stage { get; }
+}
