2b32d4be51e51eb337f95ab7b08a35b9eab2f486
Kaioru <keith@kaioru.co>
2023-08-24 17:31:13 +0800

    Add sort and gather inventory handlers

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Contracts/FieldOnPacketUserSortItemRequest.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Inventories;
+
+namespace Edelstein.Protocol.Gameplay.Game.Contracts;
+
+public record FieldOnPacketUserSortItemRequest(
+    IFieldUser User,
+    ItemInventoryType Type
+);
