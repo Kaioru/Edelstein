07ac9570a93ec157fc62ba7d1a259189c95caa7f
Kaioru <keith@kaioru.co>
2023-09-05 15:35:02 +0800

    Add UserTransferChannelRequest handlers and plugs

---
+++ src/common/Edelstein.Common.Gameplay.Game/Handlers/UserTransferChannelRequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Protocol.Gameplay.Game.Contracts;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Utilities.Packets;
+using Edelstein.Protocol.Utilities.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Game.Handlers;
+
+public class UserTransferChannelRequestHandler : AbstractPipedFieldHandler<FieldOnPacketUserTransferChannelRequest>
+{
+    public UserTransferChannelRequestHandler(IPipeline<FieldOnPacketUserTransferChannelRequest?> pipeline) : base(pipeline)
+    {
+    }
+
+    public override short Operation => (short)PacketRecvOperations.UserTransferChannelRequest;
+
+    protected override FieldOnPacketUserTransferChannelRequest? Serialize(IFieldUser user, IPacketReader reader)
+        => new(user, reader.ReadByte());
+}
