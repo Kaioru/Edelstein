807fd0009127dffeba15ca03c69cf33ce94ea822
Kaioru <keith@kaioru.co>
2023-09-05 16:20:38 +0800

    Add UserCharacterInfoRequest handlers and plugs

---
+++ src/common/Edelstein.Common.Gameplay.Game/Handlers/UserCharacterInfoRequest.cs
@@ -0,0 +1,28 @@
+﻿using Edelstein.Common.Gameplay.Packets;
+using Edelstein.Protocol.Gameplay.Game.Contracts;
+using Edelstein.Protocol.Gameplay.Game.Objects;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Utilities.Packets;
+using Edelstein.Protocol.Utilities.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Game.Handlers;
+
+public class UserCharacterInfoRequest : AbstractPipedFieldHandler<FieldOnPacketUserCharacterInfoRequest>
+{
+    public UserCharacterInfoRequest(IPipeline<FieldOnPacketUserCharacterInfoRequest?> pipeline) : base(pipeline)
+    {
+    }
+
+    public override short Operation => (short)PacketRecvOperations.UserCharacterInfoRequest;
+    
+    protected override FieldOnPacketUserCharacterInfoRequest? Serialize(IFieldUser user, IPacketReader reader)
+    {
+        _ = reader.ReadInt();
+        var objID = reader.ReadInt();
+        var obj = user.Field?.GetPool(FieldObjectType.User)?.GetObject(objID);
+
+        if (obj is not IFieldUser target) return default;
+
+        return new(user, target);
+    }
+}
