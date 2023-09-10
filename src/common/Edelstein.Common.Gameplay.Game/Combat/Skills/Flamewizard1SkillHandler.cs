c2542753f10490a653e022b34806a5a8206ef529
Kaioru <keith@kaioru.co>
2023-09-11 05:18:20 +0800

    Add flame wizard skill handlers

---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Flamewizard1SkillHandler.cs
@@ -0,0 +1,28 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects;
+using Edelstein.Protocol.Gameplay.Game.Objects.Summoned;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills;
+
+public class Flamewizard1SkillHandler : NoblesseSkillHandler
+{
+    public override int ID => Job.Flamewizard;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.FlamewizardMagicGuard:
+                context.AddTemporaryStat(TemporaryStatType.MagicGuard, context.SkillLevel!.X);
+                break;
+            case Skill.FlamewizardFlame:
+                context.AddSummoned(MoveAbilityType.Walk, SummonedAssistType.Attack);
+                break;
+        }
+        
+        return base.HandleSkillUse(context, user);
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Flamewizard2SkillHandler.cs
@@ -0,0 +1,34 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects.Mob.Stats;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills;
+
+public class Flamewizard2SkillHandler : Flamewizard1SkillHandler
+{
+    public override int ID => Job.Flamewizard2;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.FlamewizardMeditation:
+                context.SetTargetParty();
+                break;
+            case Skill.FlamewizardSlow:
+                context.SetTargetField();
+                context.AddMobTemporaryStat(MobTemporaryStatType.Speed, context.SkillLevel!.X);
+                break;
+            case Skill.FlamewizardMagicBooster:
+                context.AddTemporaryStat(TemporaryStatType.Booster, context.SkillLevel!.X);
+                break;
+            case Skill.FlamewizardElementalReset:
+                context.AddTemporaryStat(TemporaryStatType.ElementalReset, context.SkillLevel!.X);
+                break;
+        }
+
+        return base.HandleSkillUse(context, user);
+    }
+}
