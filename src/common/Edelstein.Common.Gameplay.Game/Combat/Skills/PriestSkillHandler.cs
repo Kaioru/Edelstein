1f4ce599f74fa3f41e649cca60a15b514d0ef235
Kaioru <keith@kaioru.co>
2023-09-11 05:10:27 +0800

    Add bishop skill handlers

--- src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/PriestSkillHandler.cs
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/PriestSkillHandler.cs
@@ -1,24 +1,23 @@
 ﻿using Edelstein.Common.Gameplay.Constants;
 using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects;
 using Edelstein.Protocol.Gameplay.Game.Objects.Mob;
 using Edelstein.Protocol.Gameplay.Game.Objects.Mob.Stats;
+using Edelstein.Protocol.Gameplay.Game.Objects.Summoned;
 using Edelstein.Protocol.Gameplay.Game.Objects.User;
 using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
 
 namespace Edelstein.Common.Gameplay.Game.Combat.Skills;
 
-public class MageThunderColdSkillHandler : WizardThunderColdSkillHandler
+public class PriestSkillHandler : ClericSkillHandler
 {
-    public override int ID => Job.MageThunderCold;
+    public override int ID => Job.Priest;
     
     public override Task HandleAttackMob(ISkillContext context, IFieldUser user, IFieldMob mob)
     {
         switch (context.Skill?.ID)
         {
-            case Skill.Mage2IceStrike:
-                context.AddMobTemporaryStat(MobTemporaryStatType.Freeze, 1);
-                break;
-            case Skill.Mage2ThunderSpear:
+            case Skill.PriestShiningRay:
                 if (context.Random.Next(0, 100) <= context.SkillLevel!.Prop)
                     context.AddMobTemporaryStat(MobTemporaryStatType.Stun, 1);
                 break;
@@ -31,18 +30,25 @@ public class MageThunderColdSkillHandler : WizardThunderColdSkillHandler
     {
         switch (context.Skill?.ID)
         {
-            case Skill.Mage2Seal:
+            case Skill.PriestDispel:
+                // TODO dispel
+                break;
+            case Skill.PriestMysticDoor:
+                // TODO dispel
+                break;
+            case Skill.PriestHolySymbol:
+                context.SetTargetParty();
+                context.AddTemporaryStat(TemporaryStatType.HolySymbol, context.SkillLevel!.X);
+                break;
+            case Skill.PriestDoom:
                 if (context.Random.Next(0, 100) <= context.SkillLevel!.Prop)
-                    context.AddMobTemporaryStat(MobTemporaryStatType.Seal, 1);
+                    context.AddMobTemporaryStat(MobTemporaryStatType.Doom, 1);
                 break;
-            case Skill.Mage2MagicBooster:
-                context.AddTemporaryStat(TemporaryStatType.Booster, context.SkillLevel!.X);
+            case Skill.PriestSummonDragon:
+                context.AddSummoned(MoveAbilityType.Walk, SummonedAssistType.Attack);
                 break;
-            case Skill.Mage2TeleportMastery:
+            case Skill.PriestTeleportMastery:
                 context.AddTemporaryStat(TemporaryStatType.TeleportMasteryOn, context.SkillLevel!.X, expire: DateTime.MaxValue);
-                break;
-            case Skill.Mage2ElementalReset:
-                context.AddTemporaryStat(TemporaryStatType.ElementalReset, context.SkillLevel!.X);
                 break;
         }
 
