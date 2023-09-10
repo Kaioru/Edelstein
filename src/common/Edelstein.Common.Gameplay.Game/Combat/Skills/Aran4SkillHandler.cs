88d7db7cfb1ec59212aca4007cac02bdfd246716
Kaioru <keith@kaioru.co>
2023-09-11 06:18:22 +0800

    Add aran skill handlers

---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Aran4SkillHandler.cs
@@ -0,0 +1,32 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects;
+using Edelstein.Protocol.Gameplay.Game.Objects.Summoned;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills;
+
+public class Aran4SkillHandler : Aran3SkillHandler
+{
+    public override int ID => Job.Aran4;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.AranMapleHero:
+                context.SetTargetParty();
+                context.AddTemporaryStat(TemporaryStatType.BasicStatUp, context.SkillLevel!.X);
+                break;
+            case Skill.AranFreezeStanding:
+                context.AddTemporaryStat(TemporaryStatType.Stance, context.SkillLevel!.Prop);
+                break;
+            case Skill.AranComboBarrier:
+                context.AddTemporaryStat(TemporaryStatType.ComboBarrier, context.SkillLevel!.X);
+                break;
+        }
+
+        return base.HandleSkillUse(context, user);
+    }
+}
