b0243c1a319074e3ed97ed38fceb0327fab41627
Kaioru <keith@kaioru.co>
2023-09-11 23:59:14 +0800

    Add adventurer archer skill handlers

---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Normal/ArcherSkillHandler.cs
@@ -0,0 +1,8 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Normal;
+
+public class ArcherSkillHandler : NoviceSkillHandler
+{
+    public override int ID => Job.Archer;
+}
---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Normal/CrossbowmasterSkillHandler.cs
@@ -0,0 +1,35 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects;
+using Edelstein.Protocol.Gameplay.Game.Objects.Summoned;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Normal;
+
+public class CrossbowmasterSkillHandler : SniperSkillHandler
+{
+    public override int ID => Job.Crossbowmaster;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.CrossbowmasterSharpEyes:
+                var cr = context.SkillLevel!.X;
+                var cd = context.SkillLevel!.CDMax;
+                
+                context.AddTemporaryStat(TemporaryStatType.SharpEyes, (cr << 8) + cd);
+                break;
+            case Skill.CrossbowmasterBlind:
+                context.AddTemporaryStat(TemporaryStatType.Blind, 1);
+                break;
+            case Skill.CrossbowmasterFreezer:
+                context.ResetSummoned(Skill.SniperGoldenEagle);
+                context.AddSummoned(MoveAbilityType.Fly, SummonedAssistType.Attack);
+                break;
+        }
+        
+        return base.HandleSkillUse(context, user);
+    }
+}
