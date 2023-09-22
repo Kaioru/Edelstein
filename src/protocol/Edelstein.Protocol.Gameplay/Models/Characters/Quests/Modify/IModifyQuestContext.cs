0eb66646e51b6e260ca30601d05185378e2971b0
Kaioru <keith@kaioru.co>
2023-09-23 00:53:36 +0800

    Add modifyquestcontext interface

---
+++ src/protocol/Edelstein.Protocol.Gameplay/Models/Characters/Quests/Modify/IModifyQuestContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Edelstein.Protocol.Gameplay.Models.Characters.Quests.Modify;
+
+public interface IModifyQuestContext
+{
+    void SetRecord(int questID, string record);
+    void SetRecordEx(int questID, string recordEx);
+    
+    void ResetRecord(int questID);
+    void ResetRecordEx(int questID);
+    
+    void Complete(int questID);
+    void Reset(int questID);
+}
