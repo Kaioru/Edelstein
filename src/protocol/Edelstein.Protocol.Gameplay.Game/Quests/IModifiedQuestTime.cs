3394bfc4da9cca13fcc05d4a56e83d29d886e6fe
Kaioru <keith@kaioru.co>
2023-09-24 01:23:36 +0800

    Add quest manager interfaces, modifiedquesttime manager interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/IModifiedQuestTime.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Utilities.Repositories;
+
+namespace Edelstein.Protocol.Gameplay.Game.Quests;
+
+public interface IModifiedQuestTime : IIdentifiable<int>
+{
+    DateTime DateStart { get; }
+    DateTime DateEnd { get; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/IQuestManager.cs
@@ -0,0 +1,12 @@
+﻿using Edelstein.Protocol.Gameplay.Game.Objects.User;
+
+namespace Edelstein.Protocol.Gameplay.Game.Quests;
+
+public interface IQuestManager
+{
+    Task ActStart(IFieldUser user);
+    Task ActEnd(IFieldUser user);
+    
+    Task<bool> CheckStart(IFieldUser user);
+    Task<bool> CheckEnd(IFieldUser user);
+}
