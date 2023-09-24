2f0b3d01c1113432c4b00aefff58bdd853cf90c1
Kaioru <keith@kaioru.co>
2023-09-25 03:33:46 +0800

    Add loading of quest act item info

--- src/common/Edelstein.Common.Gameplay.Game/Quests/Templates/QuestTemplateAct.cs
+++ src/common/Edelstein.Common.Gameplay.Game/Quests/Templates/QuestTemplateAct.cs
@@ -1,4 +1,5 @@
-﻿using Edelstein.Protocol.Data;
+﻿using System.Collections.Immutable;
+using Edelstein.Protocol.Data;
 using Edelstein.Protocol.Gameplay.Game.Quests.Templates;
 
 namespace Edelstein.Common.Gameplay.Game.Quests.Templates;
@@ -9,12 +10,30 @@ public record QuestTemplateAct : IQuestTemplateAct
     {
         IncEXP = property?.Resolve<int>("exp");
         IncMoney = property?.Resolve<int>("money");
+        IncPOP = property?.Resolve<int>("pop");
+        IncPetTameness = property?.Resolve<int>("pop");
+        PetSpeed = property?.Resolve<int>("petspeed");
+        BuffItemID = property?.Resolve<int>("buffitemID");
+        Info = property?.ResolveOrDefault<string>("info");
+        NPCAction = property?.ResolveOrDefault<string>("npcAct");
         
         NextQuest = property?.Resolve<int>("nextQuest");
+        
+        Items = property?.Resolve("item")?.Children
+            .Select(p => (IQuestTemplateActItem)new QuestTemplateActItem(Convert.ToInt32(p.Name), p.ResolveAll()))
+            .ToImmutableList();
     }
     
     public int? IncEXP { get; }
     public int? IncMoney { get; }
-    
+    public int? IncPOP { get; }
+    public int? IncPetTameness { get; }
+    public int? PetSpeed { get; }
+    public int? BuffItemID { get; }
+    public string? Info { get; }
+    public string? NPCAction { get; }
+
     public int? NextQuest { get; }
+    
+    public ICollection<IQuestTemplateActItem>? Items { get; }
 }
--- src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/Templates/IQuestTemplateAct.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/Templates/IQuestTemplateAct.cs
@@ -4,6 +4,16 @@ public interface IQuestTemplateAct
 {
     int? IncEXP { get; }
     int? IncMoney { get; }
+    int? IncPOP { get; }
+    int? IncPetTameness { get; }
+    int? PetSpeed { get; }
+    int? BuffItemID { get; }
+    
+    string? Info { get; }
+    
+    string? NPCAction { get; }
     
     int? NextQuest { get; }
+    
+    ICollection<IQuestTemplateActItem>? Items { get; }
 }
