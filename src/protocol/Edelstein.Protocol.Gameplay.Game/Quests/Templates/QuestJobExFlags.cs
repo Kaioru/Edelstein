283c061c20832138575c2dd07c2b0ce370e03b3e
Kaioru <keith@kaioru.co>
2023-09-25 02:35:18 +0800

    Add quest job flags

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/Templates/QuestJobExFlags.cs
@@ -0,0 +1,9 @@
+﻿namespace Edelstein.Protocol.Gameplay.Game.Quests.Templates;
+
+[Flags]
+public enum QuestJobExFlags
+{
+    Bmage = 0x1,
+    Wildhunter = 0x2,
+    Mechanic = 0x8
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Quests/Templates/QuestJobFlags.cs
@@ -0,0 +1,23 @@
+﻿namespace Edelstein.Protocol.Gameplay.Game.Quests.Templates;
+
+[Flags]
+public enum QuestJobFlags
+{
+    Novice = 0x1,
+    Swordman = 0x2,
+    Magician = 0x4,
+    Archer = 0x8,
+    Rogue = 0x10,
+    Pirate = 0x20,
+    
+    Noblesse = 0x400,
+    Soulfighter = 0x800,
+    Flamewizard = 0x1000,
+    Windbreaker = 0x2000,
+    Nightwalker = 0x4000,
+    Striker = 0x8000,
+    
+    Legend = 0x100000,
+    Aran = 0x200000,
+    Evan = 0x400000
+}
