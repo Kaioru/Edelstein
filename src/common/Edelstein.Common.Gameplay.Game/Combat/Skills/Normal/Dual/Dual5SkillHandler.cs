f2c6aec0c218d151d592395e46b79eefa258367b
Kaioru <keith@kaioru.co>
2023-10-10 14:49:48 +0800

    Add thorns skill use handling

--- src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Normal/Dual/Dual5SkillHandler.cs
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Normal/Dual/Dual5SkillHandler.cs
@@ -1,8 +1,27 @@
 ﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
 
 namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Normal.Dual;
 
 public class Dual5SkillHandler : Dual4SkillHandler
 {
     public override int ID => Job.Dual5;
+
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+
+        switch (context.Skill?.ID)
+        {
+            case Skill.Dual5ThornsEffect:
+                var cr = context.SkillLevel!.X;
+                var cd = context.SkillLevel!.CDMax;
+
+                context.AddTemporaryStat(TemporaryStatType.ThornsEffect, (cr << 8) + cd);
+                break;
+        }
+
+        return base.HandleSkillUse(context, user);
+    }
 }
