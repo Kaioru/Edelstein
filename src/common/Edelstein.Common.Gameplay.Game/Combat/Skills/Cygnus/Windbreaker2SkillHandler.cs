eeb76c2a4a5b4b55cd76aaf3e1783ae8326a78c1
Kaioru <keith@kaioru.co>
2023-10-12 19:52:54 +0800

    Add windbreaker skill handlers

---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Cygnus/Windbreaker2SkillHandler.cs
@@ -0,0 +1,30 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Cygnus;
+
+public class Windbreaker2SkillHandler : Windbreaker1SkillHandler
+{
+    public override int ID => Job.Windbreaker2;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.WindbreakerBowBooster:
+                context.AddTemporaryStat(TemporaryStatType.Booster, context.SkillLevel!.X);
+                break;
+            case Skill.WindbreakerSoulArrowBow:
+                context.AddTemporaryStat(TemporaryStatType.SoulArrow, 1);
+                break;
+            case Skill.WindbreakerWindWalk:
+                context.AddTemporaryStat(TemporaryStatType.DarkSight, context.SkillLevel!.X);
+                context.AddTemporaryStat(TemporaryStatType.Speed, -context.SkillLevel!.Y);
+                break;
+        }
+
+        return base.HandleSkillUse(context, user);
+    }
+}
---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Cygnus/Windbreaker4SkillHandler.cs
@@ -0,0 +1,15 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Cygnus;
+
+public class Windbreaker4SkillHandler : Windbreaker3SkillHandler
+{
+    public override int ID => Job.Windbreaker4;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        return base.HandleSkillUse(context, user);
+    }
+}
