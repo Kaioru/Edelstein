a48e5be9239e5572c35ca066a7890e605e7ea0a5
Kaioru <keith@kaioru.co>
2023-10-12 21:29:37 +0800

    Add viper skill handlers

---
+++ src/common/Edelstein.Common.Gameplay.Game/Combat/Skills/Normal/PirateViper2SkillHandler.cs
@@ -0,0 +1,26 @@
+﻿using Edelstein.Common.Gameplay.Constants;
+using Edelstein.Protocol.Gameplay.Game.Combat;
+using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Gameplay.Models.Characters.Stats;
+
+namespace Edelstein.Common.Gameplay.Game.Combat.Skills.Normal;
+
+public class PirateViper2SkillHandler : Pirate1SkillHandler
+{
+    public override int ID => Job.Infighter;
+    
+    public override Task HandleSkillUse(ISkillContext context, IFieldUser user)
+    {
+        switch (context.Skill?.ID)
+        {
+            case Skill.InfighterKnuckleBooster:
+                context.AddTemporaryStat(TemporaryStatType.Booster, context.SkillLevel!.X);
+                break;
+            case Skill.InfighterOakCask:
+                context.AddTemporaryStat(TemporaryStatType.Morph, context.SkillLevel!.Morph);
+                break;
+        }
+        
+        return base.HandleSkillUse(context, user);
+    }
+}
