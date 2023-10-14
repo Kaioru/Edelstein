b7996eb22d612aefb2977fec95f8df87505cf511
Kaioru <keith@kaioru.co>
2023-10-14 18:12:35 +0800

    Add EffectSquib

---
+++ src/common/Edelstein.Common.Gameplay.Game/Objects/User/Effects/SquibEffect.cs
@@ -0,0 +1,14 @@
+﻿using Edelstein.Protocol.Utilities.Packets;
+
+namespace Edelstein.Common.Gameplay.Game.Objects.User.Effects;
+
+public record SquibEffect(
+    string Path
+) : IPacketWritable
+{
+    public void WriteTo(IPacketWriter writer)
+    {
+        writer.WriteByte((byte)EffectType.SquibEffect);
+        writer.WriteString(Path);
+    }
+}
