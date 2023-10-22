4ca5d83afafa88275f3f60daac8c9742468c6d40
Kaioru <keith@kaioru.co>
2023-10-22 17:59:50 +0800

    Add mobreward interfaces

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Objects/Mob/Rewards/IMobReward.cs
@@ -0,0 +1,22 @@
+﻿namespace Edelstein.Protocol.Gameplay.Game.Objects.Mob.Rewards;
+
+public interface IMobReward
+{
+    int? ItemID { get; }
+    
+    int? Money { get; }
+    
+    int? NumberMin { get; }
+    int? NumberMax { get; }
+    
+    int? ReqQuest { get; }
+    
+    int? ReqLevelMin { get; }
+    int? ReqLevelMax { get; }
+    
+    int? ReqMobLevelMin { get; }
+    int? ReqMobLevelMax { get; }
+    
+    DateTime? DateStart { get; }
+    DateTime? DateEnd { get; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Objects/Mob/Rewards/IMobRewardManager.cs
@@ -0,0 +1,12 @@
+﻿using Edelstein.Protocol.Gameplay.Game.Objects.User;
+using Edelstein.Protocol.Utilities.Repositories.Methods;
+
+namespace Edelstein.Protocol.Gameplay.Game.Objects.Mob.Rewards;
+
+public interface IMobRewardManager
+{
+    Task Insert(int mobID, IMobReward reward);
+    Task InsertAll(IMobReward reward);
+    Task<ICollection<IMobReward>> RetrieveAll(int mobID);
+    Task<ICollection<IMobReward>> RetrieveCalculated(IFieldUser user, IFieldMob mob);
+}
