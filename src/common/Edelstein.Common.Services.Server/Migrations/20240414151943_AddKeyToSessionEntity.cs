fcb1451dcd18d46357d92fe3b36de68f96588a73
Kaioru <keith@kaioru.co>
2024-04-14 23:19:56 +0800

    Add key to sessions

---
+++ src/common/Edelstein.Common.Services.Server/Migrations/20240414151943_AddKeyToSessionEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Edelstein.Common.Services.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddKeyToSessionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Key",
+                table: "sessions",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "sessions");
+        }
+    }
+}
