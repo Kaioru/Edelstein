745a32943425d29a0feca3d92489bf88ba2a8bff
Kaioru <keith@kaioru.co>
2024-08-18 01:02:34 +0800

    Add UpdateScreenSetting base handler

---
+++ src/common/Edelstein.Common.Gameplay/Handling/Packets/BaseUpdateScreenSettingHandler.cs
@@ -0,0 +1,16 @@
+﻿using Edelstein.Protocol.Gameplay;
+using Edelstein.Protocol.Gameplay.Contracts.Packets.Recv;
+using Edelstein.Protocol.Gameplay.Handling;
+using Edelstein.Protocol.Network.Packets;
+using Edelstein.Protocol.Utilities.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Handling.Packets;
+
+public class BaseUpdateScreenSettingHandler<TStageSystem, TStageSystemUser>(
+    IPipeline<PipedPacketMessage<TStageSystem, TStageSystemUser, UpdateScreenSetting>> pipeline
+) : PipedPacketHandler<TStageSystem, TStageSystemUser, UpdateScreenSetting>(
+    (short)PacketRecvOperation.UpdateScreenSetting,
+    pipeline
+)
+    where TStageSystem : IStageSystem<TStageSystem, TStageSystemUser> 
+    where TStageSystemUser : IStageSystemUser<TStageSystem, TStageSystemUser>;
