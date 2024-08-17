07938279e56815f37689ff66a65f34886929bf68
Kaioru <keith@kaioru.co>
2024-08-17 19:09:28 +0800

    Add repository interfaces and local implementation

---
+++ src/protocol/Edelstein.Protocol.Utilities/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Edelstein.Protocol.Utilities.Repositories.Methods;
+
+namespace Edelstein.Protocol.Utilities.Repositories;
+
+public interface IReadOnlyRepository<in TKey, TEntry> :
+    IRepositoryMethodRetrieve<TKey, TEntry>,
+    IRepositoryMethodRetrieveAll<TKey, TEntry>
+    where TKey : notnull
+    where TEntry : IRepositoryEntry<TKey>;
---
+++ src/protocol/Edelstein.Protocol.Utilities/Repositories/Methods/IRepositoryMethodRetrieveAll.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edelstein.Protocol.Utilities.Repositories.Methods;
+
+public interface IRepositoryMethodRetrieveAll<in TKey, TEntry>
+    where TKey : notnull
+    where TEntry : IRepositoryEntry<TKey>
+{
+    Task<ICollection<TEntry>> RetrieveAll();
+}
