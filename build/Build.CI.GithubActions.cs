30fe8d8e96e9b97e87cccd2c57b4b319a2158599
Kaioru <keith@kaioru.co>
2024-08-18 20:51:32 +0800

    Add pack and publish github workflows

--- build/Build.CI.GithubActions.cs
+++ build/Build.CI.GithubActions.cs
@@ -1,9 +1,23 @@
 ﻿using Nuke.Common.CI.GitHubActions;
 
 [GitHubActions(
-    "Build",
+    "build",
     GitHubActionsImage.UbuntuLatest,
     On = new[] { GitHubActionsTrigger.Push },
     InvokedTargets = new[] { nameof(Compile) }
+)]
+[GitHubActions(
+    "pack",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.WorkflowDispatch },
+    InvokedTargets = new[] { nameof(Pack) },
+    FetchDepth = 0
+)]
+[GitHubActions(
+    "publish",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.WorkflowDispatch },
+    InvokedTargets = new[] { nameof(Publish) },
+    FetchDepth = 0
 )]
 partial class Build;
