e177a7d5bc46002106c610690f6cbc03f62a1bd9
Kaioru <keith@kaioru.co>
2024-08-29 00:36:48 +0800

    Add create new character structures

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Login/Contracts/Packets/Recv/CreateNewCharacter.cs
@@ -0,0 +1,23 @@
+﻿using BinarySerialization;
+using Edelstein.Protocol.Network.Packets;
+using Edelstein.Protocol.Network.Packets.Types;
+
+namespace Edelstein.Protocol.Gameplay.Login.Contracts.Packets.Recv;
+
+public record CreateNewCharacter : StructuredRecvPacket
+{
+    [FieldOrder(0)] public required LPString Name { get; init; }
+    [FieldOrder(1)] public int CurSelectedRace { get; init; }
+    [FieldOrder(2)] public short CurSelectedSubJob { get; init; }
+    
+    [FieldOrder(3)] public int Face { get; init; }
+    [FieldOrder(4)] public int Hair { get; init; }
+    [FieldOrder(5)] public int HairColor { get; init; }
+    [FieldOrder(6)] public int Skin { get; init; }
+    [FieldOrder(7)] public int Coat { get; init; }
+    [FieldOrder(8)] public int Pants { get; init; }
+    [FieldOrder(9)] public int Shoes { get; init; }
+    [FieldOrder(10)] public int Weapon { get; init; }
+    
+    [FieldOrder(11)] public byte Gender { get; init; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Login/Contracts/Packets/Send/CreateNewCharacterResult.cs
@@ -0,0 +1,24 @@
+﻿using BinarySerialization;
+using Edelstein.Protocol.Gameplay.Contracts.Packets.Shared;
+using Edelstein.Protocol.Network.Packets;
+
+namespace Edelstein.Protocol.Gameplay.Login.Contracts.Packets.Send;
+
+public record CreateNewCharacterResult() : StructuredSendPacket((short)PacketSendOperation.CreateNewCharacterResult)
+{
+    [FieldOrder(0)] 
+    public required LoginResultCode Result { get; init; }
+    
+    [FieldOrder(1)]
+    [SerializeWhen(nameof(Result), LoginResultCode.Success)]
+    public CreateNewCharacterResultInfo? Info { get; init; }
+}
+
+public record CreateNewCharacterResultInfo : StructuredBasePacket
+{
+    [FieldOrder(0)]
+    public required StructuredCharacterStat CharacterStat { get; init; }
+    
+    [FieldOrder(1)]
+    public required StructuredAvatarLook AvatarLook { get; init; }
+}
