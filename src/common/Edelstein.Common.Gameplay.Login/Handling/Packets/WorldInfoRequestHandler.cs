a00e0c6dc20d10d59753a6fa8c49c2b2eb9d2877
Kaioru <keith@kaioru.co>
2024-08-30 13:03:28 +0800

    Add LogoutWorld and WorldInfoRequest handling

---
+++ src/common/Edelstein.Common.Gameplay.Login/Handling/Packets/WorldInfoRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using Edelstein.Common.Gameplay.Handling;
+using Edelstein.Protocol.Gameplay.Handling;
+using Edelstein.Protocol.Gameplay.Login;
+using Edelstein.Protocol.Gameplay.Login.Contracts.Packets.Recv;
+using Edelstein.Protocol.Network.Packets;
+using Edelstein.Protocol.Utilities.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Login.Handling.Packets;
+
+public class WorldInfoRequestHandler(
+    IPipeline<PipedPacketMessage<ILoginStageSystem, ILoginStageSystemUser, WorldRequest>> pipeline
+) : PipedPacketHandler<ILoginStageSystem, ILoginStageSystemUser, WorldRequest>(
+    (short)PacketRecvOperation.WorldInfoRequest,
+    pipeline
+);
---
+++ src/common/Edelstein.Common.Gameplay.Login/Handling/Pipes/UserOnPacketLogoutWorld.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Handling;
+using Edelstein.Protocol.Gameplay.Login;
+using Edelstein.Protocol.Gameplay.Login.Contracts.Packets.Recv;
+using Edelstein.Protocol.Utilities.Pipelines;
+
+namespace Edelstein.Common.Gameplay.Login.Handling.Pipes;
+
+public class UserOnPacketLogoutWorld : IPipe<PipedPacketMessage<ILoginStageSystem, ILoginStageSystemUser, LogoutWorld>>
+{
+    public Task Handle(IPipelineContext ctx, PipedPacketMessage<ILoginStageSystem, ILoginStageSystemUser, LogoutWorld> message)
+    {
+        if (message.User.State != LoginState.SelectCharacter) return Task.CompletedTask;
+        
+        message.User.AccountWorldData = null;
+        message.User.State = LoginState.SelectWorld;
+        message.User.SelectedWorldID = null;
+        message.User.SelectedChannelID = null;
+
+        return Task.CompletedTask;
+    }
+}
