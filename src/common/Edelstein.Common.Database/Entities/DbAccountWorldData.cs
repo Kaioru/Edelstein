2f22be0d4026429679c5281189703625d0250f20
Kaioru <keith@kaioru.co>
2024-08-30 17:39:13 +0800

    Fix DbAccountWorldData not being a record

--- src/common/Edelstein.Common.Database/Entities/DbAccountWorldData.cs
+++ src/common/Edelstein.Common.Database/Entities/DbAccountWorldData.cs
@@ -4,7 +4,7 @@ using Edelstein.Protocol.Gameplay.Entities;
 
 namespace Edelstein.Common.Database.Entities;
 
-public class DbAccountWorldData : AccountWorldData
+public record DbAccountWorldData : AccountWorldData
 {
     public DbAccount Account { get; set; }
     public ICollection<DbCharacter> Characters { get; set; }
