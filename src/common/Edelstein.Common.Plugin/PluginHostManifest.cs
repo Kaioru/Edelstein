8935b004f5057e060f09fe8cb43baf5e73dfec98
Kaioru <keith@kaioru.co>
2024-09-15 01:43:10 +0800

    Remove dependencies from plugin host

--- src/common/Edelstein.Common.Plugin/PluginHostManifest.cs
+++ src/common/Edelstein.Common.Plugin/PluginHostManifest.cs
@@ -9,6 +9,4 @@ public record PluginHostManifest : IPluginHostManifest
     public required string Description { get; init; }
     
     public required string EntryPoint { get; init; }
-
-    public ICollection<string> Dependencies { get; init; } = new List<string>();
 }
