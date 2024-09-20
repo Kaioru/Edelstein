0c8037ab1850fbfa26d1c04a2a85d9a44720e193
Kaioru <keith@kaioru.co>
2024-09-20 21:25:10 +0800

    Combine extensions

--- src/protocol/Edelstein.Protocol.Gameplay.Game/Objects/Users/IFieldUserExtensions.cs
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Objects/Users/IFieldUserExtensions.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Threading.Tasks;
+using Edelstein.Protocol.Gameplay.Entities.Inventories.Modifiers;
+using Edelstein.Protocol.Gameplay.Entities.Modifiers;
 using Edelstein.Protocol.Gameplay.Game.Contracts.Packets.Send;
 using Edelstein.Protocol.Gameplay.Game.Objects.Users.Messages;
 using Edelstein.Protocol.Network.Packets.Types;
@@ -16,4 +19,10 @@ public static class IFieldUserExtensions
                 Chat = new LPString(chat)
             }
         });
+    
+    public static Task ModifyStats(this IFieldUser user, Action<IModifyStatContext>? action = null, bool exclRequest = false)
+        => user.Modify(m => m.Stats(action, exclRequest));
+    
+    public static Task ModifyInventory(this IFieldUser user, Action<IModifyInventoryContextGroup>? action = null, bool exclRequest = false)
+        => user.Modify(m => m.Inventory(action, exclRequest));
 }
