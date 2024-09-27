5fa6c392a41a5de281f91d676fa3f23457e0494a
Kaioru <keith@kaioru.co>
2024-09-28 02:26:19 +0800

    Add UserEmotion packets

---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Contracts/Packets/Recv/UserEmotion.cs
@@ -0,0 +1,11 @@
+﻿using BinarySerialization;
+using Edelstein.Protocol.Network.Packets;
+
+namespace Edelstein.Protocol.Gameplay.Game.Contracts.Packets.Recv;
+
+public record UserEmotion : StructuredRecvPacket
+{
+    [FieldOrder(0)] public int Emotion { get; init; }
+    [FieldOrder(1)] public int Duration { get; init; }
+    [FieldOrder(2)] public bool ByItemOption { get; init; }
+}
---
+++ src/protocol/Edelstein.Protocol.Gameplay.Game/Contracts/Packets/Send/UserEmotion.cs
@@ -0,0 +1,12 @@
+﻿using BinarySerialization;
+using Edelstein.Protocol.Network.Packets;
+
+namespace Edelstein.Protocol.Gameplay.Game.Contracts.Packets.Send;
+
+public record UserEmotion() : StructuredSendPacket((short)PacketSendOperation.UserEmotion)
+{
+    [FieldOrder(0)] public required int ObjectID { get; init; }
+    [FieldOrder(1)] public int Emotion { get; init; }
+    [FieldOrder(2)] public int Duration { get; init; }
+    [FieldOrder(3)] public bool ByItemOption { get; init; }
+}
