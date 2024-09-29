45694750e8e743a9abeb2c2affef40f444b7def5
Kaioru <keith@kaioru.co>
2024-09-29 14:53:59 +0800

    Fix overlapping fieldset contimove fields

--- src/common/Edelstein.Common.Gameplay.Game/Continent/ContiMove.cs
+++ src/common/Edelstein.Common.Gameplay.Game/Continent/ContiMove.cs
@@ -141,7 +141,7 @@ public class ContiMove : FieldSet, IContiMove
     {
         await base.Initialize(manager);
 
-        StartShipMoveField = await manager.Retrieve(Template.StartShipMoveFieldID);
+        StartShipMoveField = Register(await manager.Retrieve(Template.StartShipMoveFieldID));
         WaitField = Register(await manager.Retrieve(Template.WaitFieldID));
         MoveField = Register(await manager.Retrieve(Template.MoveFieldID));
         if (Template.CabinFieldID.HasValue)
